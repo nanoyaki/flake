37bbf80667b7d8357fb8a47b04e1c020d5686a67
nanoyaki <hanakretzer@gmail.com>
2024-05-14 22:29:24 +0200

    hana-nixos 143

--- common/configuration.nix
+++ common/configuration.nix
@@ -119,6 +119,25 @@
     #media-session.enable = true;
   };
 
+  services.pipewire.extraConfig.pipewire-pulse."92-low-latency" = {
+    context.modules = [
+      {
+        name = "libpipewire-module-protocol-pulse";
+        args = {
+          pulse.min.req = "32/48000";
+          pulse.default.req = "32/48000";
+          pulse.max.req = "32/48000";
+          pulse.min.quantum = "32/48000";
+          pulse.max.quantum = "32/48000";
+        };
+      }
+    ];
+    stream.properties = {
+      node.latency = "32/48000";
+      resample.quality = 1;
+    };
+  };
+
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 

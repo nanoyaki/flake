c74577546e53298e5369d8833e7377106dfdd178
nanoyaki <hanakretzer@gmail.com>
2024-05-14 22:13:22 +0200

    hana-nixos 140

--- common/configuration.nix
+++ common/configuration.nix
@@ -40,7 +40,7 @@
   time.timeZone = "Europe/Berlin";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.defaultLocale = "en_GB.UTF-8";
 
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "de_DE.UTF-8";
@@ -56,10 +56,10 @@
 
   i18n.supportedLocales = [
     "en_GB.UTF-8/UTF-8"
-    "C.UTF-8/UTF-8"
+    "en_US.UTF-8/UTF-8"
     "de_DE.UTF-8/UTF-8"
     "ja_JP.UTF-8/UTF-8"
-    "en_US.UTF-8/UTF-8"
+    "C.UTF-8/UTF-8"
   ];
 
   # Enable the X11 windowing system.

9683088b2a05860cea2f858aebdec4aa57dd07de
nanoyaki <hanakretzer@gmail.com>
2024-05-15 23:28:20 +0200

    hana-nixos 160

--- common/configuration.nix
+++ common/configuration.nix
@@ -122,9 +122,10 @@
     #media-session.enable = true;
   };
 
-  services.pipewire.extraConfig.pipewire."99-rates.conf" = {
+  services.pipewire.extraConfig.pipewire."92-low-latency" = {
     context.properties = {
       default.clock.rate = 48000;
+      default.clock.allowed-rates = [48000];
       default.clock.quantum = 32;
       default.clock.min-quantum = 32;
       default.clock.max-quantum = 32;

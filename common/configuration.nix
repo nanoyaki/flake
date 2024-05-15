dc9a32200c6e438bbcf25ab0e0fc01e618a9fb5d
nanoyaki <hanakretzer@gmail.com>
2024-05-15 23:23:09 +0200

    hana-nixos 159

--- common/configuration.nix
+++ common/configuration.nix
@@ -115,7 +115,7 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
-    jack.enable = true;
+    # jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
@@ -204,7 +204,7 @@
     gnome.gnome-disk-utility
     baobab
     # When pipewire.service.jack.enable is true, enable this:
-    pipewire.jack
+    # pipewire.jack
 
     # OS
     (import ./rebuild.nix {inherit pkgs;})

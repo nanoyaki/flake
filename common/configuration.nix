af88712d3f1313adba8511b997a112426cdc88f0
nanoyaki <hanakretzer@gmail.com>
2024-05-15 23:48:23 +0200

    hana-nixos 164

--- common/configuration.nix
+++ common/configuration.nix
@@ -119,34 +119,8 @@
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
-    #media-session.enable = true;
-    wireplumber.enable = true;
-  };
-
-  services.pipewire.wireplumber.configPackages = [
-    (pkgs.writeTextDir "share/wireplumber/main.lua.d/99-alsa-lowlatency.lua" ''
-      alsa_monitor.rules = {
-        {
-          matches = {{{ "node.name", "matches", "alsa_playback.*" }}};
-          apply_properties = {
-            ["audio.format"] = "S32LE",
-            ["audio.rate"] = "96000", -- for USB soundcards it should be twice your desired rate
-            ["api.alsa.period-size"] = 2, -- defaults to 1024, tweak by trial-and-error
-            -- ["api.alsa.disable-batch"] = true, -- generally, USB soundcards use the batch mode
-          },
-        },
-      }
-    '')
-  ];
-
-  services.pipewire.extraConfig.pipewire."92-low-latency" = {
-    context.properties = {
-      default.clock.rate = 48000;
-      default.clock.allowed-rates = [48000];
-      default.clock.quantum = 32;
-      default.clock.min-quantum = 32;
-      default.clock.max-quantum = 32;
-    };
+    # media-session.enable = true;
+    # wireplumber.enable = true;
   };
 
   # I HAVE NO FUCKING IDEA HOW TO MAKE OSU LAZER USE A 48K SAMPLE RATE

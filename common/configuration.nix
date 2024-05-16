c3c063c1df56eacc487ca2778db02e8c623b1328
nanoyaki <hanakretzer@gmail.com>
2024-05-16 19:26:25 +0200

    hana-nixos 169

--- common/configuration.nix
+++ common/configuration.nix
@@ -70,17 +70,17 @@
   services.displayManager.sddm.enable = true;
   #services.xserver.desktopManager.plasma5.enable = true;
   services.desktopManager.plasma6.enable = true;
-  environment.plasma6.excludePackages = with pkgs.kdePackages; [
-    konsole
-    kate
-    elisa
-    kwrited
-    kwallet
-    ark
-    okular
-    print-manager
-    # dolphin
-  ];
+  # environment.plasma6.excludePackages = with pkgs.kdePackages; [
+  #   konsole
+  #   kate
+  #   elisa
+  #   kwrited
+  #   kwallet
+  #   ark
+  #   okular
+  #   print-manager
+  #   # dolphin
+  # ];
   programs.kdeconnect.enable = false;
 
   # Configure keymap in X11

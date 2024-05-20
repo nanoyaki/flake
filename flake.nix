63f493c9cfb1097fb4f3695aa912f03834087e14
nanoyaki <hanakretzer@gmail.com>
2024-05-21 00:32:41 +0200

    hana-nixos 190

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     nixpkgs-xr.url = "github:nix-community/nixpkgs-xr";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
     catppuccin.url = "github:catppuccin/nix";
-    envision.url = "https://gitlab.com/Scrumplex/envision/-/archive/nix/envision-nix.tar";
+    envision.url = "gitlab:Scrumplex/envision/nix";
     home-manager = {
       url = "github:nix-community/home-manager/master";
       # The `follows` keyword in inputs is used for inheritance.

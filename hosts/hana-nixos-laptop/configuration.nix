8edd4a3b092b789321b8bc7d3eb681fac2cd0e6a
nanoyaki <hanakretzer@gmail.com>
2024-05-23 08:45:46 +0200

    hana-nixos-laptop 11

--- hosts/hana-nixos-laptop/configuration.nix
+++ hosts/hana-nixos-laptop/configuration.nix
@@ -8,6 +8,10 @@
 }: {
   imports = [
     ./hardware-configuration.nix
+
+    inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
+    inputs.nixos-hardware.nixosModules.common-gpu-amd
+    inputs.nixos-hardware.nixosModules.common-pc-ssd
   ];
 
   # Boot settings

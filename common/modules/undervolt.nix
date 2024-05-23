88cf36c2fd45594de91bd13ddb1a3fff258a2b74
nanoyaki <hanakretzer@gmail.com>
2024-05-23 21:14:57 +0200

    hana-nixos 204

--- common/modules/undervolt.nix
+++ common/modules/undervolt.nix
@@ -1,7 +1,5 @@
 {pkgs, ...}:
 pkgs.writeShellScriptBin "undervolt" ''
-  alias python3="${pkgs.python3}/bin/python3"
-
   # TODO: Add nix packaging for the python script
-  python3 $HOME/git-repos/Ryzen-5800x3d-linux-undervolting/ruv.py -c 8 -o -30
+  ${pkgs.python3}/bin/python3 $HOME/git-repos/Ryzen-5800x3d-linux-undervolting/ruv.py -c 8 -o -30
 ''

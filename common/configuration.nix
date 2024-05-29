063a779fe087d1ed4236c2e5cab7df01758cfd08
nanoyaki <hanakretzer@gmail.com>
2024-05-29 22:38:33 +0200

    hana-nixos 225

--- common/configuration.nix
+++ common/configuration.nix
@@ -82,6 +82,9 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
+  # Theming
+  catppuccin.enable = true;
+
   # Gnome:
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;

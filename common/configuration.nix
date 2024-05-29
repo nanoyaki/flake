0147e41104e7dee4fa46f896e41226e0abb982e7
nanoyaki <hanakretzer@gmail.com>
2024-05-29 23:11:05 +0200

    hana-nixos 228

--- common/configuration.nix
+++ common/configuration.nix
@@ -197,7 +197,7 @@
   ];
 
   # Enable automatic login for the user.
-  services.xserver.displayManager.autoLogin = {
+  services.displayManager.autoLogin = {
     enable = true;
     user = "${username}";
   };

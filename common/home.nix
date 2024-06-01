f17e828b00e8af5a7321f0d2628685a4ea5bb671
nanoyaki <hanakretzer@gmail.com>
2024-06-02 00:23:06 +0200

    hana-nixos 252

--- common/home.nix
+++ common/home.nix
@@ -102,6 +102,7 @@
   # the home Manager release notes for a list of state version
   # changes in each release.
   home.stateVersion = "24.05";
+  home-manager.backupFileExtension = "backup";
 
   # Let home Manager install and manage itself.
   programs.home-manager.enable = true;

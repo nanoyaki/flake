8ae08c22175d2e50678bc781c8901946bde6178b
nanoyaki <hanakretzer@gmail.com>
2024-06-02 01:17:26 +0200

    hana-nixos 258

--- common/configuration.nix
+++ common/configuration.nix
@@ -168,6 +168,35 @@
     # no need to redefine it in your config for now)
     # media-session.enable = true;
     # wireplumber.enable = true;
+
+    extraConfig = {
+      pipewire."92-low-latency" = {
+        context.properties = {
+          default.clock.rate = 48000;
+          default.clock.quantum = 32;
+          default.clock.min-quantum = 32;
+          default.clock.max-quantum = 32;
+        };
+      };
+      pipewire-pulse."92-low-latency" = {
+        context.modules = [
+          {
+            name = "libpipewire-module-protocol-pulse";
+            args = {
+              pulse.min.req = "32/48000";
+              pulse.default.req = "32/48000";
+              pulse.max.req = "32/48000";
+              pulse.min.quantum = "32/48000";
+              pulse.max.quantum = "32/48000";
+            };
+          }
+        ];
+        stream.properties = {
+          node.latency = "32/48000";
+          resample.quality = 1;
+        };
+      };
+    };
   };
 
   # I HAVE NO FUCKING IDEA HOW TO MAKE OSU LAZER USE A 48K SAMPLE RATE

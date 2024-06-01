e1fb9740f421b430c9fff2f1e8bfdbe4825554f3
nanoyaki <hanakretzer@gmail.com>
2024-06-02 00:29:56 +0200

    hana-nixos 252

--- hosts/hana-nixos/home.nix
+++ hosts/hana-nixos/home.nix
@@ -3,15 +3,6 @@
   pkgs,
   ...
 }: {
-  imports = [
-    # ../../common/modules/home/gamemode/gamemode.nix
-  ];
-  # dirty workaround since im dumb
-  # home.file.".config/mongodb/" = {
-  #   source = ../../common/modules/mongodb/configs;
-  #   recursive = true;
-  # };
-
   home.packages = with pkgs; [
     # Games
     protonup

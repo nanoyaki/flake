d566cfac9f48ba1d3c0e35f00e1c86643740d67e
nanoyaki <hanakretzer@gmail.com>
2024-06-15 14:46:57 +0200

    hana-nixos 274

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-xr.url = "github:nix-community/nixpkgs-xr";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
-    catppuccin.url = "github:catppuccin/nix/main";
+    catppuccin.url = "github:catppuccin/nix";
     envision.url = "gitlab:Scrumplex/envision/nix";
     home-manager = {
       url = "github:nix-community/home-manager/master";

378c3c596bbd4849557f45bb6065a40208b6da62
nanoyaki <hanakretzer@gmail.com>
2024-06-19 17:36:08 +0200

    hana-nixos 285

--- hosts/hana-nixos/configuration.nix
+++ hosts/hana-nixos/configuration.nix
@@ -14,7 +14,7 @@
     inputs.aagl.nixosModules.default
 
     inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
-    #inputs.nixos-hardware.nixosModules.common-gpu-amd
+    inputs.nixos-hardware.nixosModules.common-gpu-amd
     inputs.nixos-hardware.nixosModules.common-pc-ssd
   ];
 

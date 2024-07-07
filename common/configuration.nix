6be99aac2b22b5f2cc588208e478eba0f0f2b8ea
nanoyaki <hanakretzer@gmail.com>
2024-07-07 17:44:37 +0200

    hana-nixos 310

--- common/configuration.nix
+++ common/configuration.nix
@@ -93,9 +93,10 @@
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
+  services.displayManager.defaultSession = "plasmax11";
 
   # Plasma 6
+  services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
     konsole
@@ -116,8 +117,8 @@
   };
 
   # Theming
-  catppuccin.accent = "pink";
   catppuccin.enable = true;
+  catppuccin.accent = "pink";
   catppuccin.flavor = "frappe";
 
   console.catppuccin.enable = true;

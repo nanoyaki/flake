16f94fea3e4ffd2d1b43b6cf80e67b253ff286e6
nanoyaki <hanakretzer@gmail.com>
2024-07-09 16:52:05 +0200

    hana-nixos 314

--- hosts/hana-nixos/configuration.nix
+++ hosts/hana-nixos/configuration.nix
@@ -91,7 +91,7 @@
     extraCompatPackages = with pkgs; [
       proton-ge-bin
       (proton-ge-bin.overrideAttrs (finalAttrs: _: {
-        version = "GE-Proton9-4-rtsp7";
+        version = "GE-Proton9-9-rtsp11";
         src = pkgs.fetchzip {
           url = "https://github.com/SpookySkeletons/proton-ge-rtsp/releases/download/${finalAttrs.version}/${finalAttrs.version}.tar.gz";
           hash = "sha256-l/zt/Kv6g1ZrAzcxDNENByHfUp/fce3jOHVAORc5oy0=";

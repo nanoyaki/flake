cdbca667e06205a1956f0392f32cc262f389352e
nanoyaki <hanakretzer@gmail.com>
2024-08-03 15:43:35 +0200

    hana-nixos 384

--- common/configuration.nix
+++ common/configuration.nix
@@ -125,16 +125,35 @@
   console.keyMap = "de";
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  services.xserver = {
+    enable = true;
+
+    desktopManager = {
+      xterm.enable = false;
+    };
+
+    windowManager.i3 = {
+      enable = true;
+      extraPackages = with pkgs; [
+        dmenu #application launcher most people use
+        i3status # gives you the default i3 status bar
+        i3lock #default i3 screen locker
+        i3blocks #if you are planning on using i3blocks over i3status
+      ];
+    };
+  };
+
+  services.displayManager = {
+    autoLogin = {
+      enable = true;
+      user = "${username}";
+    };
+
+    defaultSession = "none+i3";
+  };
 
   # Desktop Manager
-  services.nano.plasma6.enable = true;
-
-  # Enable automatic login for the user.
-  services.displayManager.autoLogin = {
-    enable = true;
-    user = "${username}";
-  };
+  services.nano.plasma6.enable = false;
 
   # Input
   services.libinput.mouse.accelProfile = "flat";

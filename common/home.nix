37c98f26cbad184ac0d4c0399baf4ecf66d7791f
nanoyaki <hanakretzer@gmail.com>
2024-08-03 16:15:18 +0200

    hana-nixos 389

--- common/home.nix
+++ common/home.nix
@@ -91,6 +91,15 @@
     delta.catppuccin.enable = true;
   };
 
+  programs.neovim.coc = {
+    enable = true;
+    settings.languageserver.nix = {
+      command = "nil";
+      filetypes = ["nix"];
+      rootPatterns = ["flake.nix"];
+    };
+  };
+
   # This value determines the home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new home Manager release introduces backwards

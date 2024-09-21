cff69fad9f2b1f5fc8f13e3ce7ccc3b6779a58b9
nanoyaki <hanakretzer@gmail.com>
2024-09-21 22:47:56 +0200

    hana-nixos 457 de9b01

--- common/modules/audio.nix
+++ common/modules/audio.nix
@@ -39,7 +39,7 @@ in {
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
-      # jack.enable = true;
+      jack.enable = true;
 
       # use the example session manager (no others are packaged yet so this is enabled by default,
       # no need to redefine it in your config for now)

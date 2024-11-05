9b7083ebebd7361e8e0df1b2598dd7b3b911bb01
nanoyaki <hanakretzer@gmail.com>
2024-11-05 13:47:27 +0100

    feat(cpu): undervolt cpu on kuroyuri

--- nixosConfigurations/kuroyuri/hardware/cpu.nix
+++ nixosConfigurations/kuroyuri/hardware/cpu.nix
@@ -1,7 +1,41 @@
-{ ... }:
+{ lib, pkgs, ... }:
 
 {
-  boot.kernelModules = [ "kvm-amd" ];
+  boot.kernelModules = [
+    "kvm-amd"
+    "msr"
+  ];
 
   hardware.cpu.amd.updateMicrocode = true;
+
+  services.power-profiles-daemon.enable = true;
+
+  systemd.services.amdctl-undervolt =
+    let
+      amdctl = lib.getExe (
+        pkgs.amdctl.overrideAttrs {
+          patches = [
+            ./max-vid.patch
+          ];
+        }
+      );
+    in
+    {
+      enable = true;
+      description = "Undervolt by ~30 milivolts";
+
+      wantedBy = [ "multi-user.target" ];
+
+      script = ''
+        ${amdctl} -m
+        ${amdctl} -p0 -v196
+        ${amdctl} -p1 -v176
+        ${amdctl} -p2 -v156
+      '';
+
+      serviceConfig = {
+        User = "root";
+        Group = "wheel";
+      };
+    };
 }

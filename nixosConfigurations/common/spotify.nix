0dd75d090c9ff4b483873a825b668c13bb87df00
nanoyaki <hanakretzer@gmail.com>
2024-11-14 22:09:17 +0100

    fix(spotify): switch from spotifyd to librespot

--- nixosConfigurations/common/spotify.nix
+++ nixosConfigurations/common/spotify.nix
@@ -1,16 +1,8 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
-  hm = {
-    home.packages = [ pkgs.spotify-qt ];
-
-    services.spotifyd = {
-      enable = true;
-
-      settings.global = {
-        username = "aex77xiuiva5s17odjzngj6jb";
-        password_cmd = "cat ${config.sops.secrets."spotify/password".path}";
-      };
-    };
-  };
+  hm.home.packages = [
+    pkgs.spotify-qt
+    pkgs.librespot
+  ];
 }

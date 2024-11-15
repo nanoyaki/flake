7488b71108628ec1f0a9e6b27358c42900f2f400
nanoyaki <hanakretzer@gmail.com>
2024-11-15 21:51:08 +0100

    fix(spotify): add systemd service for librespot

--- nixosConfigurations/common/spotify.nix
+++ nixosConfigurations/common/spotify.nix
@@ -1,8 +1,43 @@
-{ pkgs, ... }:
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
+
+let
+  # String -> String
+  toUppercase =
+    str:
+    (lib.strings.toUpper (builtins.substring 0 1 str))
+    + builtins.substring 1 (builtins.stringLength str) str;
+in
 
 {
-  hm.home.packages = [
-    pkgs.spotify-qt
-    pkgs.librespot
-  ];
+  hm.home.packages = [ pkgs.spotify-qt ];
+
+  systemd.user.services.librespot = {
+    enable = true;
+    description = "Librespot";
+
+    unitConfig.ConditionUser = "!root";
+
+    script = ''
+      ${lib.getExe pkgs.librespot} --username "aex77xiuiva5s17odjzngj6jb" \
+        --cache $XDG_CACHE_HOME/librespot \
+        --enable-oauth \
+        --name "${toUppercase config.networking.hostName}" \
+        --bitrate 320 \
+        --device-type "computer"
+    '';
+
+    serviceConfig = {
+      Restart = "no";
+      Type = "simple";
+    };
+
+    wantedBy = [ "multi-user.target" ];
+
+    restartTriggers = [ pkgs.librespot ];
+  };
 }

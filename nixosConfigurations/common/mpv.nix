56ac7ec2447edffabaa59969fa67dd40baa66424
nanoyaki <hanakretzer@gmail.com>
2024-11-18 16:16:59 +0100

    feat(mpv): use default mime map

--- nixosConfigurations/common/mpv.nix
+++ nixosConfigurations/common/mpv.nix
@@ -1,5 +1,7 @@
 {
   lib,
+  nLib,
+  pkgs,
   config,
   ...
 }:
@@ -9,10 +11,67 @@ let
     types
     mkOption
     mkIf
-    mkMerge
     ;
+  inherit (nLib) mapDefaultForMimeTypes;
 
   cfg = config.modules.mpv;
+
+  defaultAudioApp = mapDefaultForMimeTypes [
+    "audio/aac"
+    "audio/ac3"
+    "audio/AMR"
+    "audio/AMR-WB"
+    "audio/ape"
+    "audio/basic"
+    "audio/flac"
+    "audio/midi"
+    "audio/mp4"
+    "audio/mpeg"
+    "audio/ogg"
+    "audio/opus"
+    "audio/vnd.dts"
+    "audio/vnd.dts.hd"
+    "audio/x-aiff"
+    "audio/x-ape"
+    "audio/x-flac"
+    "audio/x-matroska"
+    "audio/x-mpegurl"
+    "audio/x-ms-wma"
+    "audio/x-musepack"
+    "audio/x-pn-realaudio"
+    "audio/x-scpls"
+    "audio/x-speex"
+    "audio/x-tta"
+    "audio/x-wav"
+    "audio/x-wavpack"
+    "audio/x-xm"
+  ] pkgs.mpv;
+  defaultVideoApp = mapDefaultForMimeTypes [
+    "video/3gpp"
+    "video/3gpp2"
+    "video/annodex"
+    "video/avi"
+    "video/divx"
+    "video/flv"
+    "video/h264"
+    "video/mp2t"
+    "video/mp4"
+    "video/mpeg"
+    "video/mpeg2"
+    "video/msvideo"
+    "video/ogg"
+    "video/quicktime"
+    "video/x-flv"
+    "video/x-matroska"
+    "video/x-mng"
+    "video/x-ms-asf"
+    "video/x-ms-wmv"
+    "video/x-msvideo"
+    "video/x-nsv"
+    "video/vnd.mpegurl"
+    "video/webm"
+    "video/x-ogm+ogg"
+  ] pkgs.mpv;
 in
 
 {
@@ -30,64 +89,9 @@ in
 
   config = {
     hm.programs.mpv.enable = true;
-
-    xdg.mime.defaultApplications = mkMerge [
-      (mkIf cfg.defaultAudioPlayer {
-        "audio/aac" = "mpv.desktop";
-        "audio/ac3" = "mpv.desktop";
-        "audio/AMR" = "mpv.desktop";
-        "audio/AMR-WB" = "mpv.desktop";
-        "audio/ape" = "mpv.desktop";
-        "audio/basic" = "mpv.desktop";
-        "audio/flac" = "mpv.desktop";
-        "audio/midi" = "mpv.desktop";
-        "audio/mp4" = "mpv.desktop";
-        "audio/mpeg" = "mpv.desktop";
-        "audio/ogg" = "mpv.desktop";
-        "audio/opus" = "mpv.desktop";
-        "audio/vnd.dts" = "mpv.desktop";
-        "audio/vnd.dts.hd" = "mpv.desktop";
-        "audio/x-aiff" = "mpv.desktop";
-        "audio/x-ape" = "mpv.desktop";
-        "audio/x-flac" = "mpv.desktop";
-        "audio/x-matroska" = "mpv.desktop";
-        "audio/x-mpegurl" = "mpv.desktop";
-        "audio/x-ms-wma" = "mpv.desktop";
-        "audio/x-musepack" = "mpv.desktop";
-        "audio/x-pn-realaudio" = "mpv.desktop";
-        "audio/x-scpls" = "mpv.desktop";
-        "audio/x-speex" = "mpv.desktop";
-        "audio/x-tta" = "mpv.desktop";
-        "audio/x-wav" = "mpv.desktop";
-        "audio/x-wavpack" = "mpv.desktop";
-        "audio/x-xm" = "mpv.desktop";
-      })
-      (mkIf cfg.defaultVideoPlayer {
-        "video/3gpp" = "mpv.desktop";
-        "video/3gpp2" = "mpv.desktop";
-        "video/annodex" = "mpv.desktop";
-        "video/avi" = "mpv.desktop";
-        "video/divx" = "mpv.desktop";
-        "video/flv" = "mpv.desktop";
-        "video/h264" = "mpv.desktop";
-        "video/mp2t" = "mpv.desktop";
-        "video/mp4" = "mpv.desktop";
-        "video/mpeg" = "mpv.desktop";
-        "video/mpeg2" = "mpv.desktop";
-        "video/msvideo" = "mpv.desktop";
-        "video/ogg" = "mpv.desktop";
-        "video/quicktime" = "mpv.desktop";
-        "video/vnd.mpegurl" = "mpv.desktop";
-        "video/webm" = "mpv.desktop";
-        "video/x-flv" = "mpv.desktop";
-        "video/x-matroska" = "mpv.desktop";
-        "video/x-mng" = "mpv.desktop";
-        "video/x-ms-asf" = "mpv.desktop";
-        "video/x-ms-wmv" = "mpv.desktop";
-        "video/x-msvideo" = "mpv.desktop";
-        "video/x-nsv" = "mpv.desktop";
-        "video/x-ogm+ogg" = "mpv.desktop";
-      })
-    ];
+    xdg.mime.defaultApplications =
+      (mkIf cfg.defaultAudioPlayer defaultAudioApp) // (mkIf cfg.defaultVideoPlayer defaultVideoApp);
+    hm.xdg.mimeApps.defaultApplications =
+      (mkIf cfg.defaultAudioPlayer defaultAudioApp) // (mkIf cfg.defaultVideoPlayer defaultVideoApp);
   };
 }

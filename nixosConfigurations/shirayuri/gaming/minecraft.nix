42cb8d06a3b76bb6ee9b56ccebf06c8e52c5f26f
nanoyaki <hanakretzer@gmail.com>
2024-11-24 21:13:03 +0100

    chore(minecraft): switch to stable prismlauncher for now

--- nixosConfigurations/shirayuri/gaming/minecraft.nix
+++ nixosConfigurations/shirayuri/gaming/minecraft.nix
@@ -1,11 +1,13 @@
-{ inputs', ... }:
+{ pkgs, ... }:
 
 {
-
   nix.settings.trusted-substituters = [ "https://prismlauncher.cachix.org" ];
   nix.settings.trusted-public-keys = [
     "prismlauncher.cachix.org-1:9/n/FGyABA2jLUVfY+DEp4hKds/rwO+SCOtbOkDzd+c="
   ];
 
-  environment.systemPackages = [ inputs'.prismlauncher.packages.prismlauncher ];
+  environment.systemPackages = [
+    # inputs'.prismlauncher.packages.prismlauncher
+    pkgs.prismlauncher
+  ];
 }

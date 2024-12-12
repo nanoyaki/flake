ae6cee63fc26edb199d72a161886ccdec61fe408
nanoyaki <hanakretzer@gmail.com>
2024-12-12 01:34:33 +0100

    feat(git): add gpg key for signing commits

--- configs/shirayuri/git.nix
+++ configs/shirayuri/git.nix
@@ -1,15 +1,33 @@
+{ lib, pkgs, ... }:
+
 let
   name = "nanoyaki";
   email = "hanakretzer@gmail.com";
+  signingkey = "4682C5CB4D9DEA3C";
 in
 
 {
   hm.programs.git = {
     userName = name;
     userEmail = email;
+
+    aliases = {
+      co = "checkout";
+    };
+
+    signing = {
+      key = signingkey;
+
+      gpgPath = "${lib.getExe pkgs.gnupg}";
+      signByDefault = true;
+    };
   };
 
-  programs.git.config.user = {
-    inherit name email;
+  programs.git.config = {
+    user = {
+      inherit name email signingkey;
+    };
+
+    commit.gpgsign = true;
   };
 }

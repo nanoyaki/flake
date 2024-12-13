ec198d62047db2986a640e4b546ff9468f8f7b66
nanoyaki <hanakretzer@gmail.com>
2024-12-13 17:47:41 +0100

    fix(dynamicdns): add theless.one

--- configs/thelessone/servers/dynamicdns.nix
+++ configs/thelessone/servers/dynamicdns.nix
@@ -5,18 +5,32 @@
     ../../../modules/dynamicdns
   ];
 
-  sec."dynamicdns/nanoyaki.space".owner = config.services.namecheapDynDns.user;
+  sec = {
+    "dynamicdns/nanoyaki.space".owner = config.services.namecheapDynDns.user;
+    "dynamicdns/theless.one".owner = config.services.namecheapDynDns.user;
+  };
 
   services.namecheapDynDns = {
     enable = true;
 
-    domains."nanoyaki.space" = {
-      subdomains = [
-        "*"
-        "@"
-      ];
+    domains = {
+      "nanoyaki.space" = {
+        subdomains = [
+          "*"
+          "@"
+        ];
 
-      passwordFile = config.sec."dynamicdns/nanoyaki.space".path;
+        passwordFile = config.sec."dynamicdns/nanoyaki.space".path;
+      };
+
+      "theless.one" = {
+        subdomains = [
+          "*"
+          "@"
+        ];
+
+        passwordFile = config.sec."dynamicdns/theless.one".path;
+      };
     };
   };
 }

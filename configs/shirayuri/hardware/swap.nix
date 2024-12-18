f0cf209867c65f3a2ca70810fc453d689f8b73db
nanoyaki <hanakretzer@gmail.com>
2024-12-18 01:25:36 +0100

    feat(swap): add multiple swap files

--- configs/shirayuri/hardware/swap.nix
+++ configs/shirayuri/hardware/swap.nix
@@ -1,9 +1,17 @@
 {
   swapDevices = [
     {
-      device = "/swapfile";
+      device = "/var/lib/swap/swapfile32";
       size = 32 * 1024;
     }
+    # {
+    #   device = "/var/lib/swap/swapfile16";
+    #   size = 16 * 1024;
+    # }
+    # {
+    #   device = "/var/lib/swap/swapfile8";
+    #   size = 8 * 1024;
+    # }
   ];
 
   zramSwap.enable = true;

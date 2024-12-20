584ebbe3328229d9fe554fc1a5df510e9680b727
nanoyaki <hanakretzer@gmail.com>
2024-12-20 07:27:59 +0100

    feat(yuri): add private directory for myself

--- configs/yuri/samba.nix
+++ configs/yuri/samba.nix
@@ -1,11 +1,29 @@
+{ config, ... }:
+
 {
+  sec."nixos/users/hana" = { };
+
   users.users.nas.extraGroups = [ "nas" ];
   users.groups.nas = { };
 
-  systemd.tmpfiles.settings."10-samba"."/mnt/shares/Public".d = {
-    user = "nas";
-    group = "nas";
-    mode = "0755";
+  users.groups.hana = { };
+  users.users.hana = {
+    group = "hana";
+    hashedPasswordFile = config.sec."nixos/users/hana".path;
+  };
+
+  systemd.tmpfiles.settings."10-samba" = {
+    "/mnt/shares/Public".d = {
+      user = "nas";
+      group = "nas";
+      mode = "0755";
+    };
+
+    "/mnt/shares/Hana".d = {
+      user = "hana";
+      group = "hana";
+      mode = "0755";
+    };
   };
 
   services.samba = {
@@ -13,7 +31,17 @@
     openFirewall = true;
 
     settings = {
-      global.security = "user";
+      global = {
+        security = "user";
+        "netbios name" = "home-nas";
+        "server string" = "NixOS NAS";
+
+        "hosts allow" = "192.168.0. 127.0.0.1 localhost";
+        "hosts deny" = "0.0.0.0/0";
+
+        "guest account" = "nobody";
+        "map to guest" = "bad user";
+      };
 
       public = {
         path = "/mnt/shares/Public";
@@ -24,6 +52,17 @@
         "directory mask" = "0755";
         "force user" = "nas";
         "force group" = "nas";
+      };
+
+      hana = {
+        path = "/mnt/shares/Hana";
+        "browseable" = "yes";
+        "read only" = "no";
+        "guest ok" = "no";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+        "force user" = "hana";
+        "force group" = "hana";
       };
     };
   };

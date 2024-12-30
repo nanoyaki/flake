7d17d859c2f158ef4998e6e811d099153386bd68
nanoyaki <hanakretzer@gmail.com>
2024-12-30 14:24:25 +0100

    fix(terminal): simplify ghostty config

--- configs/common/terminal.nix
+++ configs/common/terminal.nix
@@ -19,41 +19,39 @@ in
     enableKitty = mkEnableOption "kitty";
   };
 
-  config =
-    {
-      hm.programs = {
-        kitty = {
-          enable = cfg.enableKitty;
+  config = {
+    hm.programs = {
+      kitty = {
+        enable = cfg.enableKitty;
 
-          font = {
-            package = pkgs.cascadia-code;
-            name = "Cascadia Mono";
-            size = 13;
-          };
-        };
-
-        alacritty = {
-          enable = true;
-
-          settings.terminal.shell = lib.mkIf config.hm.programs.zellij.enable {
-            program = "zellij";
-            args = [
-              "attach"
-              "default"
-              "-c"
-            ];
-          };
+        font = {
+          package = pkgs.cascadia-code;
+          name = "Cascadia Mono";
+          size = 13;
         };
       };
-    }
-    // mkIf cfg.enableGhostty {
-      nixpkgs.overlays = [ ghostty.overlays.default ];
 
-      nix.settings = {
-        trusted-substituters = [ "https://ghostty.cachix.org" ];
-        trusted-public-keys = [ "ghostty.cachix.org-1:QB389yTa6gTyneehvqG58y0WnHjQOqgnA+wBnpWWxns=" ];
+      alacritty = {
+        enable = true;
+
+        settings.terminal.shell = lib.mkIf config.hm.programs.zellij.enable {
+          program = "zellij";
+          args = [
+            "attach"
+            "default"
+            "-c"
+          ];
+        };
       };
-
-      environment.systemPackages = [ pkgs.ghostty ];
     };
+
+    nixpkgs.overlays = [ ghostty.overlays.default ];
+
+    nix.settings = {
+      trusted-substituters = [ "https://ghostty.cachix.org" ];
+      trusted-public-keys = [ "ghostty.cachix.org-1:QB389yTa6gTyneehvqG58y0WnHjQOqgnA+wBnpWWxns=" ];
+    };
+
+    environment.systemPackages = [ (mkIf cfg.enableGhostty pkgs.ghostty) ];
+  };
 }

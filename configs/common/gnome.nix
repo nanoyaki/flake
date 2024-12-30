1922a5f4ed207fc4b819db4abeb115519c82471b
nanoyaki <hanakretzer@gmail.com>
2024-12-30 13:52:03 +0100

    feat: add gnome module

---
+++ configs/common/gnome.nix
@@ -0,0 +1,50 @@
+{ pkgs, username, ... }:
+
+{
+  services.xserver = {
+    displayManager = {
+      gdm.enable = true;
+      autoLogin.enable = true;
+      autoLogin.user = username;
+    };
+
+    desktopManager.gnome.enable = true;
+  };
+
+  systemd.services = {
+    "getty@tty1".enable = false;
+    "autovt@tty1".enable = false;
+  };
+
+  environment.gnome.excludePackages = with pkgs; [
+    orca
+    evince
+    geary
+    gnome-disk-utility
+    gnome-backgrounds
+    gnome-tour
+    gnome-user-docs
+    baobab
+    epiphany
+    gnome-text-editor
+    gnome-calculator
+    gnome-calendar
+    gnome-characters
+    gnome-console
+    gnome-contacts
+    gnome-font-viewer
+    gnome-logs
+    gnome-maps
+    gnome-music
+    gnome-weather
+    nautilus
+    gnome-connections
+    simple-scan
+    snapshot
+    totem
+    yelp
+    gnome-software
+  ];
+
+  environment.systemPackages = [ pkgs.dolphin ];
+}

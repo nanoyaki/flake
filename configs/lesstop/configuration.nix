5b92f2af05de210ba90b278c9213f17c0e31e840
nanoyaki <hanakretzer@gmail.com>
2024-12-30 17:49:24 +0100

    feat(lesstop): easy update script

--- configs/lesstop/configuration.nix
+++ configs/lesstop/configuration.nix
@@ -1,4 +1,9 @@
-{ lib, config, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 
 {
   programs.git.enable = true;
@@ -7,6 +12,16 @@
   users.users.thelessone.hashedPasswordFile =
     lib.mkForce
       config.sec."nixos/users/thelessone-lesstop".path;
+
+  environment.systemPackages = [
+    (pkgs.writeShellScriptBin "update" ''
+      find ~ -name "*home-bac" | xargs rm
+      pushd $FLAKE_DIR
+      git pull
+      sudo nixos-rebuild switch --flake $FLAKE_DIR
+      popd
+    '')
+  ];
 
   system.stateVersion = "25.05";
 }

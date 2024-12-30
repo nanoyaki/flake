da908d4d2a1a27ee1f346e54e2780e03f464378f
nanoyaki <hanakretzer@gmail.com>
2024-12-30 14:49:56 +0100

    fix(lesstop): use different password

--- configs/lesstop/configuration.nix
+++ configs/lesstop/configuration.nix
@@ -1,5 +1,12 @@
+{ lib, config, ... }:
+
 {
   programs.git.enable = true;
+
+  sec."nixos/users/thelessone-lesstop".neededForUsers = true;
+  users.users.thelessone.hashedPasswordFile =
+    lib.mkForce
+      config.sec."nixos/users/thelessone-lesstop".path;
 
   system.stateVersion = "25.05";
 }

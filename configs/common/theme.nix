537036f7c20f1cbb256b6ac3b36c7e545209dd79
nanoyaki <hanakretzer@gmail.com>
2024-12-30 15:49:18 +0100

    fix(theme): use correct fonts

--- configs/common/theme.nix
+++ configs/common/theme.nix
@@ -25,8 +25,11 @@ in
   stylix = {
     enable = true;
 
-    cursor.package = pkgs.rose-pine-cursor;
-    cursor.name = "BreezeX-RosePine-Linux";
+    cursor = {
+      package = pkgs.rose-pine-cursor;
+      name = "BreezeX-RosePine-Linux";
+      size = 32;
+    };
 
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-${catppuccin.flavor}.yaml";
     polarity = "dark";
@@ -34,6 +37,35 @@ in
     image = pkgs.fetchurl {
       url = "https://cdn.bsky.app/img/feed_fullsize/plain/did:plc:n3xxlxmlutbyeih4rphvn5o3/bafkreie6qpaxgmgbelgddjezoqknolhqvhtwdpeq4ucfbup35oytb5i3ma@png";
       hash = "sha256-b9z6cs9hkaC1iC4oU5S7iYIYvfroPhepehHf3aLXFoc=";
+    };
+
+    fonts = {
+      serif = {
+        name = "Noto Sans";
+        package = pkgs.noto-fonts-cjk-sans;
+      };
+
+      sansSerif = {
+        name = "Noto Sans";
+        package = pkgs.noto-fonts-cjk-sans;
+      };
+
+      monospace = {
+        name = "Cascadia Mono";
+        package = pkgs.cascadia-code;
+      };
+
+      emoji = {
+        name = "Twitter Color Emoji";
+        package = pkgs.twemoji-color-font;
+      };
+
+      sizes = {
+        applications = 10;
+        terminal = 12;
+        desktop = 9;
+        popups = 9;
+      };
     };
   };
 
--- configs/lesstop/theme.nix
+++ configs/lesstop/theme.nix
@@ -7,18 +7,13 @@
 {
   imports = [ ../common/theme.nix ];
 
-  stylix = lib.mkForce {
-    enable = true;
-
-    cursor.package = pkgs.rose-pine-cursor;
-    cursor.name = "BreezeX-RosePine-Linux";
-
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/pasque.yaml";
-    polarity = "dark";
-
-    image = pkgs.fetchurl {
-      url = "https://na55l3zepb4kcg0zryqbdnay.theless.one/nix.png";
-      hash = "sha256-G2a9UxqPXQNj+sLxDKwkT5D8/6W6rWWoQFL23jXxsrU=";
-    };
+  stylix = {
+    base16Scheme = lib.mkForce "${pkgs.base16-schemes}/share/themes/pasque.yaml";
+    image = lib.mkForce (
+      pkgs.fetchurl {
+        url = "https://na55l3zepb4kcg0zryqbdnay.theless.one/nix.png";
+        hash = "sha256-G2a9UxqPXQNj+sLxDKwkT5D8/6W6rWWoQFL23jXxsrU=";
+      }
+    );
   };
 }

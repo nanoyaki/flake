b91cadbad9bb47eba48621433e48255904607063
nanoyaki <hanakretzer@gmail.com>
2025-01-03 19:44:04 +0100

    chore(configs/shirayuri/transmission): set port explicitly

--- configs/shirayuri/transmission.nix
+++ configs/shirayuri/transmission.nix
@@ -12,8 +12,12 @@ in
 {
   services.transmission = {
     enable = true;
+
     webHome = pkgs.flood-for-transmission;
-    settings.download-dir = "/mnt/os-shared/Torrents";
+    settings = {
+      download-dir = "/mnt/os-shared/Torrents";
+      rpc-port = 9091;
+    };
   };
 
   systemd.tmpfiles.settings."10-transmission".${cfg.settings.download-dir}.d = {

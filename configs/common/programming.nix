e0df9f2d71a4c0269813ac736934d1cd9b7c8849
nanoyaki <hanakretzer@gmail.com>
2025-02-04 22:54:01 +0100

    chore: remove hugo

--- configs/common/programming.nix
+++ configs/common/programming.nix
@@ -1,19 +1,10 @@
 {
-  pkgs,
-  ...
-}:
-
-{
   hm.programs.git.enable = true;
 
   programs.git = {
     enable = true;
     lfs.enable = true;
   };
-
-  environment.systemPackages = with pkgs; [
-    hugo
-  ];
 
   programs.direnv.enable = true;
 }

f3159b7e5c081cb1d07c20584b7557d1a0a6af1b
nanoyaki <hanakretzer@gmail.com>
2025-02-07 16:56:43 +0100

    fix: unset LC_ALL

--- configs/common/required/locale.nix
+++ configs/common/required/locale.nix
@@ -5,8 +5,8 @@ let
 
   cfg = config.nanoflake.localization;
 
-  languagePreference =
-    if builtins.isString cfg.language then cfg.language else lib.strings.intersperse ":" cfg.language;
+  characterSet = builtins.elemAt (lib.strings.splitString "." cfg.locale) 1;
+  posixLocalePredicate = lang: (builtins.match "^[a-z]{2}_[A-Z]{2}$" lang) != null;
 in
 
 {
@@ -34,7 +34,6 @@ in
       example = "en_GB.UTF-8";
       description = ''
         The locale for everything but messages.
-        Sets all LC_* variables apart from LC_ALL
       '';
     };
 
@@ -47,6 +46,17 @@ in
   };
 
   config = {
+    assertions = [
+      {
+        assertion =
+          if builtins.isString cfg.language then
+            posixLocalePredicate cfg.language
+          else
+            lib.lists.any posixLocalePredicate cfg.language;
+        message = "The nanoflake.localization.language option must contain the country code like in the posix locale";
+      }
+    ];
+
     time.timeZone = cfg.timezone;
 
     i18n = {
@@ -54,8 +64,13 @@ in
 
       extraLocaleSettings =
         {
-          LANGUAGE = languagePreference;
-          LC_MESSAGES = languagePreference;
+          LANGUAGE =
+            if builtins.isString cfg.language then cfg.language else lib.strings.intersperse ":" cfg.language;
+          LC_MESSAGES =
+            if builtins.isString cfg.language then
+              "${cfg.language}.${characterSet}"
+            else
+              "${lib.lists.findFirst posixLocalePredicate "en_US" cfg.language}.${characterSet}";
         }
         // builtins.listToAttrs (
           builtins.map (lcKey: lib.nameValuePair lcKey cfg.locale) [
@@ -68,10 +83,27 @@ in
             "LC_PAPER"
             "LC_TELEPHONE"
             "LC_TIME"
+            "LC_COLLATE"
+            "LC_CTYPE"
           ]
         );
 
-      supportedLocales = cfg.extraLocales;
+      supportedLocales = lib.unique (
+        (builtins.map
+          (l: (lib.replaceStrings [ "utf8" "utf-8" "UTF8" ] [ "UTF-8" "UTF-8" "UTF-8" ] l) + "/UTF-8")
+          (
+            [
+              "C.UTF-8"
+              "en_US.UTF-8"
+              config.i18n.defaultLocale
+            ]
+            ++ (lib.attrValues (lib.filterAttrs (n: v: n != "LANGUAGE") config.i18n.extraLocaleSettings))
+          )
+        )
+        ++ cfg.extraLocales
+      );
     };
+
+    environment.sessionVariables.LC_ALL = "";
   };
 }

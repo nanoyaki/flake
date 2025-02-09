e99f4bfa25f9943962eef81a586fdbce8a95c441
nanoyaki <hanakretzer@gmail.com>
2025-02-09 23:29:30 +0100

    fix: add GSETTINGS_SCHEMA_DIR env var for alcom

--- configs/common/optional/vr/vrchat.nix
+++ configs/common/optional/vr/vrchat.nix
@@ -10,5 +10,7 @@
     alcom
   ];
 
+  environment.sessionVariables.GSETTINGS_SCHEMA_DIR = "${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}/glib-2.0/schemas";
+
   hm.xdg.autostart.entries = [ "${pkgs.vrcx}/share/applications/vrcx.desktop" ];
 }

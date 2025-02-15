581571aa06bdf8e0e08fbb5b931021b7d1f765aa
nanoyaki <hanakretzer@gmail.com>
2025-02-15 15:45:01 +0100

    feat(ssh): add at server

--- configs/shirayuri/ssh.nix
+++ configs/shirayuri/ssh.nix
@@ -1,10 +1,22 @@
 { config, ... }:
 
+let
+  identityFile = config.hm.sec."private_keys/id_ume".path;
+in
+
 {
-  hm.programs.ssh.matchBlocks.nas = {
-    user = "nas";
-    host = "nas";
-    hostname = "192.168.8.101";
-    identityFile = config.hm.sec."private_keys/id_ume".path;
+  hm.programs.ssh.matchBlocks = {
+    nas = {
+      user = "nas";
+      host = "nas";
+      hostname = "192.168.8.101";
+      inherit identityFile;
+    };
+
+    at = {
+      user = "thelessone";
+      hostname = "theless.one";
+      inherit identityFile;
+    };
   };
 }

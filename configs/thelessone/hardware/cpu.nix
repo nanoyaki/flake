6c17d81a361856fc6e88a51e11a260d146d7755d
nanoyaki <hanakretzer@gmail.com>
2025-02-16 11:49:25 +0100

    fix(thelessone): disable vermeer undervolt

--- configs/thelessone/hardware/cpu.nix
+++ configs/thelessone/hardware/cpu.nix
@@ -1,19 +1,3 @@
-{ inputs, ... }:
-
-let
-  inherit (inputs) vermeer-undervolt;
-in
-
 {
-  imports = [
-    vermeer-undervolt.nixosModules.vermeer-undervolt
-  ];
-
   hardware.cpu.amd.updateMicrocode = true;
-
-  services.vermeer-undervolt = {
-    enable = true;
-    cores = 8;
-    milivolts = 30;
-  };
 }

96c6d1af687201fb30c9c341fe1f0c719e971fa5
nanoyaki <hanakretzer@gmail.com>
2025-02-20 14:59:22 +0100

    fix: use alsa-lib-with-plugins to fix libasound_module_pcm_pulse.so not found

--- configs/shirayuri/gaming/rpgmaker.nix
+++ configs/shirayuri/gaming/rpgmaker.nix
@@ -4,8 +4,10 @@
 }:
 
 let
+  nwjs = pkgs.nwjs.override { alsa-lib = pkgs.alsa-lib-with-plugins; };
+
   nwjs-run = pkgs.writeShellScriptBin "nwjs-run" ''
-    LD_PRELOAD=${pkgs.nwjs-ffmpeg-prebuilt}/lib/libffmpeg.so ${pkgs.nwjs}/bin/nw "$@"
+    LD_PRELOAD=${pkgs.nwjs-ffmpeg-prebuilt}/lib/libffmpeg.so ${nwjs}/bin/nw "$@"
   '';
 in
 

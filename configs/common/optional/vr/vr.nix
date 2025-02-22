e78556f6ae5b7e0f5e1a41bf85c6afa047521ba0
nanoyaki <hanakretzer@gmail.com>
2025-02-23 00:17:38 +0100

    feat: remove override in favor of nixpkgs-xr

--- configs/common/optional/vr/vr.nix
+++ configs/common/optional/vr/vr.nix
@@ -15,15 +15,7 @@
     hrtf = true
   '';
 
-  programs.steam.extraCompatPackages = [
-    (pkgs.proton-ge-rtsp-bin.overrideAttrs rec {
-      version = "GE-Proton9-22-rtsp17-1";
-      src = pkgs.fetchzip {
-        url = "https://github.com/SpookySkeletons/proton-ge-rtsp/releases/download/${version}/${version}.tar.gz";
-        hash = "sha256-GeExWNW0J3Nfq5rcBGiG2BNEmBg0s6bavF68QqJfuX8=";
-      };
-    })
-  ];
+  programs.steam.extraCompatPackages = [ pkgs.proton-ge-rtsp-bin ];
 
   environment.systemPackages = with pkgs; [
     # index_camera_passthrough

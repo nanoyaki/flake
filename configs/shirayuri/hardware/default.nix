645316e2e879595794df385dd2301d18ca055186
nanoyaki <hanakretzer@gmail.com>
2025-02-27 16:58:12 +0100

    feat: turn off rgb

--- configs/shirayuri/hardware/default.nix
+++ configs/shirayuri/hardware/default.nix
@@ -9,6 +9,7 @@
     ./swap.nix
     ./cooling.nix
     ./scarlett-solo.nix
+    ./norgb.nix
 
     inputs.nixos-hardware.nixosModules.common-cpu-amd
     inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
---
+++ configs/shirayuri/hardware/norgb.nix
@@ -0,0 +1,31 @@
+{ lib, pkgs, ... }:
+
+let
+  openrgb = pkgs.openrgb-with-all-plugins;
+in
+
+{
+  services.udev.packages = [ openrgb ];
+  boot.kernelModules = [
+    "i2c-dev"
+    "i2c-piix4"
+  ];
+  hardware.i2c.enable = true;
+
+  systemd.services.no-rgb = {
+    description = "no-rgb";
+
+    serviceConfig = {
+      ExecStart = pkgs.writeShellScript "no-rgb" ''
+        NUM_DEVICES=$(${lib.getExe openrgb} --noautoconnect --list-devices | grep -E '^[0-9]+: ' | wc -l)
+
+        for i in $(seq 0 $(($NUM_DEVICES - 1))); do
+          ${lib.getExe openrgb} --noautoconnect --device $i --mode static --color 000000
+        done
+      '';
+      Type = "oneshot";
+    };
+
+    wantedBy = [ "multi-user.target" ];
+  };
+}

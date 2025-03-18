59111a66dcf97c9fecfdc92a500a2b78f916865b
nanoyaki <hanakretzer@gmail.com>
2025-03-18 01:07:51 +0100

    feat: update discord-events-to-ics to v1.1.0

--- flake.nix
+++ flake.nix
@@ -61,7 +61,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     discord-events-to-ics = {
-      url = "github:nanoyaki/discord-events-to-ics/v1.0.1";
+      url = "github:nanoyaki/discord-events-to-ics/v1.1.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };

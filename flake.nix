18a694bfd83e5120a2951a8efee6869d09ebaf47
nanoyaki <hanakretzer@gmail.com>
2025-03-19 22:04:21 +0100

    chore(flake): update discord-events-to-ics to v1.1.2

--- flake.nix
+++ flake.nix
@@ -61,7 +61,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     discord-events-to-ics = {
-      url = "github:nanoyaki/discord-events-to-ics/v1.1.1";
+      url = "github:nanoyaki/discord-events-to-ics/v1.1.2";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };

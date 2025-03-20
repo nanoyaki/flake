d2305fe00860b517c393dcd3dac49455726d4069
nanoyaki <hanakretzer@gmail.com>
2025-03-20 23:13:39 +0100

    fix(immich): disable broken cuda test

--- configs/thelessone/servers/immich.nix
+++ configs/thelessone/servers/immich.nix
@@ -1,4 +1,17 @@
 {
+  nixpkgs.overlays = [
+    (final: prev: {
+      immich-machine-learning = prev.immich-machine-learning.overrideAttrs {
+        disabledTests = [
+          # Fails when onnxruntime has cudaSupport=true https://github.com/NixOS/nixpkgs/issues/352113
+          # when running a version that fails this test I see no issues using immich with cuda
+          # https://github.com/immich-app/immich/blob/2c88ce8559160a020d72aec753f8c4dc0128ef1c/machine-learning/app/test_main.py#L241
+          "test_sets_default_sess_options"
+        ];
+      };
+    })
+  ];
+
   services.immich = {
     enable = true;
     port = 2283;

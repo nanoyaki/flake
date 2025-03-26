f8a16ab108fb8800b001042e71937be247353ce0
nanoyaki <hanakretzer@gmail.com>
2025-03-26 21:00:52 +0100

    feat(minecraft): add distant horizons

--- configs/thelessone/servers/minecraft/servers.nix
+++ configs/thelessone/servers/minecraft/servers.nix
@@ -22,6 +22,8 @@ let
 
     operators.nanoyaki = "433b63b5-5f77-4a9f-b834-8463d520500c";
   };
+
+  inherit (pkgs) fetchurl;
 in
 
 {
@@ -43,29 +45,33 @@ in
       symlinks = {
         "mods" = pkgs.linkFarmFromDrvs "mods" (
           builtins.attrValues {
-            FabricApi = pkgs.fetchurl {
+            FabricApi = fetchurl {
               url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/bQZpGIz0/fabric-api-0.119.2%2B1.21.4.jar";
               sha512 = "bb8de90d5d1165ecc17a620ec24ce6946f578e1d834ddc49f85c2816a0c3ba954ec37e64f625a2f496d35ac1db85b495f978a402a62bbfcc561795de3098b5c9";
             };
-            SimpleVoiceChat = pkgs.fetchurl {
+            SimpleVoiceChat = fetchurl {
               url = "https://cdn.modrinth.com/data/9eGKb6K1/versions/pl9FpaYJ/voicechat-fabric-1.21.4-2.5.26.jar";
               sha512 = "c262302256a708d5ecb3e2d61de74bfb600b8892a5ef2780a309fff296f4e0123f6b95e10fd9823b5b2e4c532d0f013d94c87be0ddaba3423a5296ba1a7ed119";
             };
-            VeryManyPlayers = pkgs.fetchurl {
+            VeryManyPlayers = fetchurl {
               url = "https://cdn.modrinth.com/data/wnEe9KBa/versions/61Gy0NAD/vmp-fabric-mc1.21.4-0.2.0%2Bbeta.7.192-all.jar";
               sha512 = "4e13cfbb97099784bb27fbb87eebc163974ba6c31081829d01ae435920d2604df03c625d5daecbefa1a0cca40b699840d5e964819993b451b1c9c7a7bd7c80d2";
             };
-            Lithium = pkgs.fetchurl {
+            Lithium = fetchurl {
               url = "https://cdn.modrinth.com/data/gvQqBUqZ/versions/969795RH/lithium-fabric-0.15.1%2Bmc1.21.4.jar";
               sha512 = "f3dfb0810b2ddf1b430fc206be7c40453310b91efe9c82ab88d998e8707dd081e5e9cbbd44df125a2d43a418466216e5df87e34389f54b4aae9caf19df4382c9";
             };
-            Chunky = pkgs.fetchurl {
+            Chunky = fetchurl {
               url = "https://cdn.modrinth.com/data/fALzjamp/versions/VkAgASL1/Chunky-Fabric-1.4.27.jar";
               sha512 = "a89f94947e7c3992e01e46be8967d2a6593334333a546b4fff5fdb02a1f5a6b83c93adc4c72a9b9b1f14f9299efcaa8a5d7f5eeedf3da541c7e72abc5e2724c6";
             };
-            Bluemap = pkgs.fetchurl {
+            Bluemap = fetchurl {
               url = "https://cdn.modrinth.com/data/swbUV1cr/versions/Dr2hvJBc/bluemap-5.7-fabric.jar";
               sha512 = "b7483d6ff90f09258f994a6f487846c1dfe814f1d0af931bd50426fd0afc98ccad033c5f440705edc08a8ced03cf216171ba1571db2221aca4ec620d676443a4";
+            };
+            DistantHorizons = fetchurl {
+              url = "https://cdn.modrinth.com/data/uCdwusMi/versions/DTFSZmMF/DistantHorizons-neoforge-fabric-2.3.0-b-1.21.4.jar";
+              sha512 = "7337d486cde3dd43f5bed5f81277170d0dab4257f5d355e1dc88d5cfb5577a8592a35a3df80d35e1ec81b799ebc7b398c348cec6e73a0d37e7952883e49d06dd";
             };
           }
         );

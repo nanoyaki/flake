31c9bc731097e63ca7c70619092fb0231c30906f
nanoyaki <hanakretzer@gmail.com>
2025-03-30 16:33:43 +0200

    feat(deployment): use tailscale for deployment

--- configs/thelessone/deployment.nix
+++ configs/thelessone/deployment.nix
@@ -1,15 +1,20 @@
 {
   deployment = {
     targetUser = "root";
-    targetHost = "theless.one";
+    targetHost = "100.86.224.101";
     privateKeyName = "deploymentThelessone";
     publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMc3xjLJxASdTuLIrsvok5Wpm5N8TO1CI9vHt2z3oPPC";
     extraFlags = [ "--print-build-logs" ];
   };
 
   services.openssh.knownHosts = {
+    "100.86.224.101".publicKey =
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPkogFEPPOMfkRsBgyuHDQeWQMetWCZbkTpnfajTbu7t";
     "theless.one".publicKey =
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPkogFEPPOMfkRsBgyuHDQeWQMetWCZbkTpnfajTbu7t";
+
+    "100.114.189.127".publicKey =
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILp5Szm657DfoXyTuO0h25RQpPxqtYicFpboLpcL5RMb";
     "events.nanoyaki.space".publicKey =
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILp5Szm657DfoXyTuO0h25RQpPxqtYicFpboLpcL5RMb";
   };
--- configs/yuri/deployment.nix
+++ configs/yuri/deployment.nix
@@ -1,12 +1,16 @@
 {
   deployment = {
     targetUser = "root";
-    targetHost = "events.nanoyaki.space";
+    targetHost = "100.114.189.127";
     privateKeyName = "deploymentYuri";
     publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIpBykDxGMyZOdW7ECncYK9p6IseXzOnREmb9QCSG9Bn";
     extraFlags = [ "--print-build-logs" ];
   };
 
-  services.openssh.knownHosts."theless.one".publicKey =
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPkogFEPPOMfkRsBgyuHDQeWQMetWCZbkTpnfajTbu7t";
+  services.openssh.knownHosts = {
+    "100.86.224.101".publicKey =
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPkogFEPPOMfkRsBgyuHDQeWQMetWCZbkTpnfajTbu7t";
+    "theless.one".publicKey =
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPkogFEPPOMfkRsBgyuHDQeWQMetWCZbkTpnfajTbu7t";
+  };
 }

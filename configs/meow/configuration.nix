4c023db2e371811e40cf44ec3792258a991b9462
Hana Kretzer <hanakretzer@gmail.com>
2025-04-07 02:47:11 +0200

    feat(meow): add shell related configuration

--- configs/meow/configuration.nix
+++ configs/meow/configuration.nix
@@ -1,4 +1,5 @@
 {
+  lib,
   pkgs,
   username,
   config,
@@ -26,6 +27,9 @@ in
 
       vesktop
       wlx-overlay-s
+
+      meow
+      pyon
     ];
 
     stateVersion = "25.05";
@@ -60,10 +64,61 @@ in
     package = pkgs.vscodium;
   };
 
-  programs.direnv = {
-    enable = true;
-    nix-direnv.enable = true;
-    silent = true;
+  # Shell
+  programs = {
+    zsh = {
+      enable = true;
+      initExtra = ''
+        bindkey "^[[H"    beginning-of-line
+        bindkey "^[[F"    end-of-line
+        bindkey "^[[3~"   delete-char
+        bindkey "^[[1;5C" forward-word
+        bindkey "^[[1;5D" backward-word
+        bindkey "^[[3;5~" kill-word
+        bindkey "^H"      backward-kill-word
+
+        ${lib.getExe pkgs.meow}
+      '';
+
+      enableCompletion = true;
+      syntaxHighlighting.enable = true;
+      autosuggestion.enable = true;
+    };
+
+    zellij = {
+      enable = true;
+
+      enableBashIntegration = lib.mkDefault false;
+      enableZshIntegration = lib.mkDefault false;
+      enableFishIntegration = lib.mkDefault false;
+
+      settings = {
+        pane_frames = false;
+        default_layout = "compact";
+        session_serialization = false;
+      };
+    };
+
+    starship = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+    lsd = {
+      enable = true;
+      enableAliases = true;
+    };
+
+    btop.enable = true;
+    bat.enable = true;
+    fastfetch.enable = true;
+    ripgrep.enable = true;
+
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+      silent = true;
+    };
   };
 
   xdg.enable = true;

886fb9c116d076754164641b8174832eecc459ad
nanoyaki <hanakretzer@gmail.com>
2025-04-15 22:45:31 +0200

    chore: vscode -> vscodium

--- configs/common/optional/vscode.nix
+++ configs/common/optional/vscode.nix
@@ -5,44 +5,12 @@
 }:
 
 let
-  vscodePkg = pkgs.vscode-with-extensions.override {
-    vscodeExtensions =
-      with pkgs.vscode-extensions;
-      [
-        jnoortheen.nix-ide
-        catppuccin.catppuccin-vsc
-        catppuccin.catppuccin-vsc-icons
-        eamodio.gitlens
-        ms-vscode-remote.remote-ssh
-        editorconfig.editorconfig
-        yzhang.markdown-all-in-one
-        # rust-lang.rust-analyzer
-        tamasfe.even-better-toml
-        github.vscode-github-actions
-        redhat.vscode-yaml
-      ]
-      ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-        {
-          name = "discord-vscode";
-          publisher = "icrawl";
-          version = "5.8.0";
-          sha256 = "sha256-IU/looiu6tluAp8u6MeSNCd7B8SSMZ6CEZ64mMsTNmU=";
-        }
-        {
-          name = "language-hugo-vscode";
-          publisher = "budparr";
-          version = "1.3.1";
-          sha256 = "sha256-9dp8/gLAb8OJnmsLVbOAKAYZ5whavPW2Ak+WhLqEbJk=";
-        }
-      ];
-  };
-
-  exec = lib.getExe vscodePkg;
+  exec = lib.getExe pkgs.vscodium;
 in
 
 {
   environment.systemPackages = [
-    vscodePkg
+    pkgs.vscodium
   ];
 
   environment.variables = {

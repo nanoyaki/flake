eb9c74878d625fc4a0c7273b77c2ba56edc0c8c9
nanoyaki <hanakretzer@gmail.com>
2025-04-21 12:40:39 +0200

    feat(minecraft): remove custom paintings, add image2map

--- configs/thelessone/servers/minecraft/servers.nix
+++ configs/thelessone/servers/minecraft/servers.nix
@@ -114,9 +114,9 @@ in
               url = "https://cdn.modrinth.com/data/VSNURh3q/versions/EzvMx6b2/c2me-fabric-mc1.21.4-0.3.1.3.0.jar";
               sha512 = "f944bf4319cfa6fb645d0cbe807b82c74784f44ef7ac75273efa161be4625aa80390ec8cf32a232c0ebce0d0cb23b090979019d93e7550771de56d09d920dd13";
             };
-            CustomPaintings = fetchurl {
-              url = "https://cdn.modrinth.com/data/25H65ZiW/versions/LXLf2ToN/custom-paintings-1.1.1.jar";
-              sha512 = "2a21105d6e149453a9102c9f1bc72cec816bc120d566d1fbc5937fbf4d79f293b37197e92a0ed519b42228b0fd48bdb6bd29def9d093e52ab1f00be31b7365ba";
+            Image2Map = fetchurl {
+              url = "https://cdn.modrinth.com/data/13RpG7dA/versions/kkdbWskW/image2map-0.8.0%2B1.21.3.jar";
+              sha512 = "fab2fcca41a8d5e39ef48bfd557c4bfb4d27383d7bc9415c76b2100f52e34ae3bb6a5f9919a6617c2a081a886b9f869ff2c626a376e7b54a9f5491d14a66b54c";
             };
           }
         );

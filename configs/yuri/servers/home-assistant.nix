40b8e38155a6c46f3c60927fcaff44356feb70a9
nanoyaki <hanakretzer@gmail.com>
2025-05-02 01:44:20 +0200

    fix(yuri/homeassistant): configure for reverse proxy

--- configs/yuri/servers/home-assistant.nix
+++ configs/yuri/servers/home-assistant.nix
@@ -18,10 +18,17 @@
       # Includes dependencies for a basic setup
       # https://www.home-assistant.io/integrations/default_config/
       default_config = { };
-      http.server_host = [
-        "127.0.0.1"
-        "::1"
-      ];
+      http = {
+        server_host = [
+          "127.0.0.1"
+          "::1"
+        ];
+        trusted_proxies = [
+          "127.0.0.1"
+          "::1"
+        ];
+        use_x_forwarded_for = true;
+      };
     };
   };
 

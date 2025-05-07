8b1e6b7c37a0da805545c3d6d41d093d8a3efee0
nanoyaki <hanakretzer@gmail.com>
2025-05-07 22:00:42 +0200

    fix(rofi): change desktop entry to applications

--- configs/common/optional/rofi.nix
+++ configs/common/optional/rofi.nix
@@ -16,18 +16,24 @@
 
     home.packages = [ pkgs.rofi-power-menu ];
 
-    programs.plasma.shortcuts."services/rofi-run.desktop"."_launch" = "Meta";
+    programs.plasma.shortcuts."rofi-run.desktop"._launch = "Meta+S";
 
-    xdg.desktopEntries.rofi-run = {
-      name = "Rofi";
-      comment = "Application launcher";
-      exec = "${lib.getExe config.hm.programs.rofi.finalPackage} -show drun";
-      icon = "${pkgs.catppuccin-papirus-folders}/share/icons/Papirus/64x64/apps/nix-snowflake.svg";
-      terminal = false;
-      categories = [
-        "Utility"
-        "System"
-      ];
-    };
+    xdg.dataFile."applications/rofi-run.desktop".source = "${
+      pkgs.makeDesktopItem rec {
+        name = "rofi";
+        desktopName = "Rofi";
+        genericName = comment;
+        comment = "Application launcher";
+        exec = "${lib.getExe config.hm.programs.rofi.finalPackage} -show drun";
+        icon = "${pkgs.catppuccin-papirus-folders}/share/icons/Papirus/64x64/apps/nix-snowflake.svg";
+        startupNotify = true;
+        startupWMClass = "rofi";
+        terminal = false;
+        categories = [
+          "Utility"
+          "System"
+        ];
+      }
+    }/share/applications/rofi.desktop";
   };
 }

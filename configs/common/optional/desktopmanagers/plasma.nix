318f0e0cc45a7a73fe009803a6695ca889b26141
nanoyaki <hanakretzer@gmail.com>
2025-05-13 01:43:49 +0200

    feat(configs/plasma): add ocr

--- configs/common/optional/desktopmanagers/plasma.nix
+++ configs/common/optional/desktopmanagers/plasma.nix
@@ -1,5 +1,6 @@
 {
   lib,
+  lib',
   pkgs,
   config,
   username,
@@ -9,11 +10,12 @@
 
 let
   inherit (lib)
-    mkOption
     mkEnableOption
     mkIf
-    types
+    getExe
+    getExe'
     ;
+  inherit (lib') mkEnabledOption;
 
   inherit (inputs) plasma-manager;
 
@@ -22,13 +24,11 @@ in
 
 {
   options.nanoflake.desktop.plasma6 = {
-    enableWaylandDefault = mkOption {
-      type = types.bool;
-      default = true;
-      description = "Set Wayland as the default session";
-    };
+    enableWaylandDefault = mkEnabledOption "Wayland as the default session";
 
     withKdeConnect = mkEnableOption "KDE connect";
+
+    withOcrShortcut = mkEnabledOption "the ocr shortcut";
   };
 
   config = {
@@ -65,14 +65,34 @@ in
     home-manager.sharedModules = [ plasma-manager.homeManagerModules.plasma-manager ];
 
     hm = {
-      xdg.portal = {
-        extraPortals = [ pkgs.kdePackages.xdg-desktop-portal-kde ];
-        config.common.default = [ "kde" ];
-      };
+      xdg =
+        {
+          portal = {
+            extraPortals = [ pkgs.kdePackages.xdg-desktop-portal-kde ];
+            config.common.default = [ "kde" ];
+          };
+        }
+        // lib.optionalAttrs cfg.withOcrShortcut {
+          desktopEntries.ocr-ja = {
+            name = "OCR image: jpn";
+            exec = "${pkgs.writeShellScript "ocr" ''
+              IMAGE_FILE="/tmp/ocr-$RANDOM-tmp.png"
+              TEXT_FILE="/tmp/ocr-$RANDOM-tmp"
+
+              ${getExe pkgs.kdePackages.spectacle} -r -e -S -n -b -o $IMAGE_FILE || exit 1
+              ${getExe pkgs.tesseract} -l "jpn" $IMAGE_FILE $TEXT_FILE
+              ${getExe pkgs.translate-shell} -b -s japanese -t english "$(cat "$TEXT_FILE.txt")" > "$TEXT_FILE.txt"
+              ${getExe' pkgs.wl-clipboard "wl-copy"} < "$TEXT_FILE.txt"
+
+              rm $IMAGE_FILE "$TEXT_FILE.txt"
+            ''}";
+          };
+        };
 
       programs.plasma = {
         enable = true;
 
+        shortcuts."ocr-ja.desktop"._launch = "Ctrl+Print";
         shortcuts.kwin = {
           "Expose" = "Meta+Tab";
           "Maximize Window" = "Meta+Up";
@@ -92,10 +112,6 @@ in
             "kscreenlockerrc"."Daemon"."Timeout" = 10;
 
             "kdeglobals"."KDE"."AnimationDurationFactor" = 0;
-          }
-          // lib.attrsets.optionalAttrs (config.i18n.inputMethod.type == "fcitx5") {
-            "kwinrc"."Wayland"."InputMethod\[$e\]" =
-              "/run/current-system/sw/share/applications/fcitx5-wayland-launcher.desktop";
           }
           // lib.attrsets.optionalAttrs config.hm.programs.alacritty.enable {
             "kdeglobals"."General"."TerminalApplication" = "alacritty";
--- configs/common/optional/rofi.nix
+++ configs/common/optional/rofi.nix
@@ -16,24 +16,12 @@
 
     home.packages = [ pkgs.rofi-power-menu ];
 
-    programs.plasma.shortcuts."rofi-run.desktop"._launch = "Meta+S";
-
-    xdg.dataFile."applications/rofi-run.desktop".source = "${
-      pkgs.makeDesktopItem rec {
-        name = "rofi";
-        desktopName = "Rofi";
-        genericName = comment;
-        comment = "Application launcher";
-        exec = "${lib.getExe config.hm.programs.rofi.finalPackage} -show drun";
-        icon = "${pkgs.catppuccin-papirus-folders}/share/icons/Papirus/64x64/apps/nix-snowflake.svg";
-        startupNotify = true;
-        startupWMClass = "rofi";
-        terminal = false;
-        categories = [
-          "Utility"
-          "System"
-        ];
-      }
-    }/share/applications/rofi.desktop";
+    programs.plasma.shortcuts."rofi.desktop"._launch = "Meta+S";
+    xdg.desktopEntries.rofi = {
+      name = "Rofi";
+      exec = "${lib.getExe config.hm.programs.rofi.finalPackage} -show drun";
+      icon = "${pkgs.catppuccin-papirus-folders}/share/icons/Papirus/64x64/apps/nix-snowflake.svg";
+      type = "Application";
+    };
   };
 }

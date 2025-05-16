a764653e4ad2829b8bdd429ebcc604714b1406a3
nanoyaki <hanakretzer@gmail.com>
2025-05-16 18:14:57 +0200

    feat(thelessone/minecraft): add Markussd1 to the whitelist, seperate whitelist from servers file

---
+++ configs/thelessone/servers/minecraft/whitelist.nix
@@ -0,0 +1,20 @@
+{
+  Rascal1934 = "fe111f4f-6936-422c-b282-53a9a660f2b5";
+  nanoyaki = "433b63b5-5f77-4a9f-b834-8463d520500c";
+  Angreiferr = "885ca84d-669f-4cd7-a7a8-273d94fb7cd4";
+  NoWAY5 = "9760028f-eaeb-4699-8a46-a204f5b1feac";
+  einfach_calle = "3210afd0-4620-4120-9f49-d5379bf8e0b2";
+  sleepyLeyla = "8f9f8556-53fb-4ed0-b1d8-aa9d5078d170";
+  SleeperLuLu = "bcd341a1-8bfc-498b-a1e7-9fdd06e28860";
+  StinkySoks = "d333b68f-f970-42cd-a054-90c000c00404";
+  AQuuRious = "a1631188-b4b7-43a0-8828-04e63c602418";
+  LiamKia = "a177db00-c53e-428b-b468-edda01775bab";
+  wayne_gretzky = "435568af-9165-4b43-939d-a3f731742d43";
+  PhillipTheLord1 = "9a030337-eb2b-4acc-9fef-ec941dd6454c";
+  LenaSchnena = "9a82cd5c-98fd-413a-a3e2-6390cf7d4080";
+  aronachannel = "7f914413-e8df-475f-88d6-cfd3d63da375";
+  Desert__Igel = "1c19db01-9d0b-43ea-ad7b-7fc254445298";
+  MaruMaruKyun = "252d0e91-5592-428d-baf0-b432190fc0ee";
+  PooPooManPan = "93a9b9ea-d121-430b-945b-92e1a9dd5811";
+  Markussd1 = "b13a157a-2960-4e15-a68a-3de1bb64d4f0";
+}

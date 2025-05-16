8be93fc169d8565a34f153fe3e1bd217a59c1e5d
nanoyaki <hanakretzer@gmail.com>
2025-05-16 12:27:00 +0200

    chore: disable networkmanager

--- configs/common/required/networking.nix
+++ configs/common/required/networking.nix
@@ -1,8 +1,6 @@
 { lib, username, ... }:
 
 {
-  networking.networkmanager.enable = true;
-
   networking.useDHCP = lib.mkDefault true;
 
   users.users.${username}.extraGroups = [ "networkmanager" ];

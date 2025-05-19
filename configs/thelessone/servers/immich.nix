989eacf9f95a67f6be6a7c1399e3df1f39a74a43
nanoyaki <hanakretzer@gmail.com>
2025-05-19 20:48:38 +0200

    fix(thelessone/immich): use correct subdomain

--- configs/thelessone/servers/immich.nix
+++ configs/thelessone/servers/immich.nix
@@ -3,6 +3,8 @@
 {
   services.immich.accelerationDevices = [ "/dev/dri/renderD128" ];
 
+  services.media-easify.services.immich.subdomain = "immich.vpn";
+
   services.headscale.settings.dns.extra_records = lib.singleton {
     name = "immich.vpn.theless.one";
     type = "A";

8f393fc57145a37ce60829638f6ed5c6d61184c2
nanoyaki <hanakretzer@gmail.com>
2025-05-22 00:35:40 +0200

    feat(thelessone/mounts): add raid devices

--- configs/thelessone/hardware/mounts.nix
+++ configs/thelessone/hardware/mounts.nix
@@ -3,4 +3,14 @@
     device = "/dev/disk/by-uuid/711ead47-e4f7-4ef4-b7bf-daac6220243a";
     fsType = "ext4";
   };
+
+  fileSystems."/mnt/raid" = {
+    device = "/dev/disk/by-uuid/a707cee3-3b90-4b19-9b4b-7f0f3454e49c";
+    fsType = "btrfs";
+    options = [
+      "defaults"
+      "compress=zstd"
+      "space_cache=v2"
+    ];
+  };
 }

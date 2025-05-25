9f9b29106137e7bc8708ff99378d706e9f288d54
nanoyaki <hanakretzer@gmail.com>
2025-05-25 19:30:05 +0200

    feat(shirayuri): add mount for yuri

--- configs/shirayuri/hardware/disks.nix
+++ configs/shirayuri/hardware/disks.nix
@@ -1,9 +1,16 @@
-{ inputs, ... }:
+{
+  inputs,
+  pkgs,
+  config,
+  ...
+}:
 
 {
   imports = [
     inputs.disko.nixosModules.disko
   ];
+
+  sec.cifsCredentials = { };
 
   disko.devices.disk = {
     nixos = {
@@ -63,5 +70,21 @@
         };
       };
     };
+  };
+
+  environment.systemPackages = [ pkgs.cifs-utils ];
+  fileSystems."/mnt/yuri" = {
+    device = "//10.0.0.3/hana";
+    fsType = "cifs";
+    options = [
+      "noauto"
+      "x-systemd.automount"
+      "x-systemd.idle-timeout=60"
+      "x-systemd.device-timeout=5s"
+      "x-systemd.mount-timeout=5s"
+      "credentials=${config.sec.cifsCredentials.path}"
+      "uid=1000"
+      "gid=100"
+    ];
   };
 }

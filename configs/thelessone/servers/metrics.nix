8b472b77938406b3aa4ac633ce70d4489e18822e
nanoyaki <hanakretzer@gmail.com>
2025-06-04 17:45:33 +0200

    fix(thelessone/metrics): remove deprecated options from loki configuration

--- configs/thelessone/servers/metrics.nix
+++ configs/thelessone/servers/metrics.nix
@@ -189,7 +189,6 @@ in
         max_chunk_age = "1h";
         chunk_target_size = 999999;
         chunk_retain_period = "30s";
-        max_transfer_retries = 0;
       };
 
       schema_config.configs = [
@@ -223,8 +222,6 @@ in
         reject_old_samples_max_age = "168h";
       };
 
-      chunk_store_config.max_look_back_period = "0s";
-
       table_manager = {
         retention_deletes_enabled = false;
         retention_period = "0s";
@@ -232,7 +229,6 @@ in
 
       compactor = {
         working_directory = dataDir;
-        shared_store = "filesystem";
         compactor_ring.kvstore.store = "inmemory";
       };
     };

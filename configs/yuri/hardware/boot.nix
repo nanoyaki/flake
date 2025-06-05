ca9c3b35601bd51e25e129f97f7700626f56174a
nanoyaki <hanakretzer@gmail.com>
2025-06-05 19:10:27 +0200

    feat(yuri): disable integrated graphics module

--- configs/yuri/hardware/boot.nix
+++ configs/yuri/hardware/boot.nix
@@ -1,3 +1,5 @@
+{ lib, config, ... }:
+
 {
   boot = {
     initrd.availableKernelModules = [
@@ -9,7 +11,16 @@
       "sr_mod"
     ];
     kernelModules = [ "kvm-intel" ];
+    blacklistedKernelModules = [ "i915" ];
+    kernelParams = [ "i915.modeset=0" ];
 
     loader.systemd-boot.enable = true;
+  };
+
+  specialisation.graphical.configuration.boot = {
+    blacklistedKernelModules = lib.mkForce (
+      lib.filter (param: param != "i915") config.boot.blacklistedKernelModules
+    );
+    kernelParams = lib.mkForce (lib.filter (param: param != "i915.modeset=0") config.boot.kernelParams);
   };
 }

44297a8dfbd20fd9d52872a7c67647574b358498
nanoyaki <hanakretzer@gmail.com>
2025-06-07 17:05:52 +0200

    feat(thelessone/mounts): add nas mount

--- configs/thelessone/hardware/mounts.nix
+++ configs/thelessone/hardware/mounts.nix
@@ -13,4 +13,18 @@
       "space_cache=v2"
     ];
   };
+
+  fileSystems."/mnt/nas" = {
+    device = "//192.168.178.91/public";
+    fsType = "cifs";
+    options = [
+      "noauto"
+      "x-systemd.automount"
+      "x-systemd.idle-timeout=60"
+      "x-systemd.device-timeout=5s"
+      "x-systemd.mount-timeout=5s"
+      "uid=1000"
+      "gid=100"
+    ];
+  };
 }

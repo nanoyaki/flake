5874b1a7883b9a177c9f19789f09b68931146e3e
nanoyaki <hanakretzer@gmail.com>
2025-06-11 19:17:35 +0200

    feat(pkgs): add automatic updates for whisparr

--- pkgs/whisparr/default.nix
+++ pkgs/whisparr/default.nix
@@ -1,13 +1,16 @@
 {
+  lib,
   whisparr,
-  fetchurl,
+
+  _sources,
 }:
 
-whisparr.overrideAttrs rec {
-  version = "3.0.0.1038";
-  src = fetchurl {
-    name = "whisparr-x64-linux-${version}.tar.gz";
-    url = "https://whisparr.servarr.com/v1/update/eros/updatefile?version=${version}&os=linux&runtime=netcore&arch=x64";
-    sha256 = "sha256-Mn3Gbgh3xni9JsM5dvKerTJAJv6sOot32826Ku5CbO4=";
-  };
-}
+let
+  inherit (lib) optionalAttrs versionOlder;
+in
+
+whisparr.overrideAttrs (
+  optionalAttrs (versionOlder whisparr.version _sources.whisparr.version) {
+    inherit (_sources.whisparr) version src;
+  }
+)

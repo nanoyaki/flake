30c92d7b849ea523eb67459c604c4c223c96bd66
nanoyaki <hanakretzer@gmail.com>
2025-06-16 01:15:05 +0200

    feat(thelessone/shoko): don't use dynamic user

--- configs/thelessone/servers/shoko.nix
+++ configs/thelessone/servers/shoko.nix
@@ -1,9 +1,27 @@
-{ self, ... }:
+{
+  self,
+  lib,
+  config,
+  ...
+}:
 
 {
   imports = [ self.nixosModules.shoko ];
 
   services.shoko.enable = true;
+
+  systemd.services.shoko.serviceConfig = {
+    DynamicUser = lib.mkForce false;
+    User = "shoko";
+    Group = config.services'.lab-config.arr.group;
+  };
+
+  users.users.shoko = {
+    isSystemUser = true;
+    inherit (config.services'.lab-config.arr) group;
+    home = config.systemd.services.shoko.environment.SHOKO_HOME;
+    homeMode = builtins.toString config.systemd.services.shoko.serviceConfig.StateDirectoryMode;
+  };
 
   services'.caddy.reverseProxies."shoko.vpn.theless.one" = {
     port = 8111;

97e1f25ad2ba237fbcba94562f03878b950cfaff
nanoyaki <hanakretzer@gmail.com>
2025-06-18 13:44:30 +0200

    feat(thelessone): migrate to nas mass storage

--- configs/thelessone/hardware/mounts.nix
+++ configs/thelessone/hardware/mounts.nix
@@ -1,23 +1,14 @@
 {
   boot.supportedFilesystems.nfs = true;
 
-  fileSystems."/mnt/nas-raid" = {
+  fileSystems."/mnt/raid" = {
     device = "192.168.178.91:/moon";
     fsType = "nfs";
+    options = [ "nfsvers=4.2" ];
   };
 
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/711ead47-e4f7-4ef4-b7bf-daac6220243a";
     fsType = "ext4";
-  };
-
-  fileSystems."/mnt/raid" = {
-    device = "/dev/disk/by-uuid/a707cee3-3b90-4b19-9b4b-7f0f3454e49c";
-    fsType = "btrfs";
-    options = [
-      "defaults"
-      "compress=zstd"
-      "space_cache=v2"
-    ];
   };
 }

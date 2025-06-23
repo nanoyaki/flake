c006f83f88fa6ddb5bdd05ea8469013bac842686
nanoyaki <hanakretzer@gmail.com>
2025-06-23 17:29:54 +0200

    feat(kuroyuri): add programs

--- configs/kuroyuri/configuration.nix
+++ configs/kuroyuri/configuration.nix
@@ -4,6 +4,8 @@
   system.stateVersion = "24.05";
   hm.home.stateVersion = "24.11";
 
+  networking.networkmanager.enable = true;
+
   nanoflake = {
     localization.language = [
       "en_GB"
@@ -13,6 +15,17 @@
 
     firefox.enablePolicies = true;
   };
+
+  environment.systemPackages = with pkgs; [
+    jq
+    meow
+    pyon
+    nvtopPackages.amd
+    wl-clipboard
+    gimp
+    feishin
+    grayjay
+  ];
 
   hm.home.file.".face.icon".source = pkgs.fetchurl {
     url = "https://cdn.bsky.app/img/avatar/plain/did:plc:majihettvb7ieflgmkvujecu/bafkreib6be5oip6rht4vqnmldx5hzulr6irh55yarwbmxt2us2imfoiyd4@png";

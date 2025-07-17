461bb3a021c8d64a57bf684d63698dde1f6355f2
nanoyaki <hanakretzer@gmail.com>
2025-07-17 17:42:26 +0200

    feat(thelessone/minecraft): remove backup messages

--- configs/thelessone/servers/minecraft/backups.nix
+++ configs/thelessone/servers/minecraft/backups.nix
@@ -1,5 +1,4 @@
 {
-  lib,
   pkgs,
   config,
   ...
@@ -26,38 +25,6 @@
       Persistent = true;
       RandomizedDelaySec = "30s";
     };
-
-    backupPrepareCommand = lib.getExe (
-      pkgs.writeShellApplication {
-        name = "backupPrepareCommandSmp";
-        runtimeInputs = with pkgs; [
-          coreutils-full
-          tmux
-        ];
-        text = ''
-          systemctl is-active minecraft-server-smp.service --quiet && \
-          date +%s > /tmp/minecraftServerSmpBackupStartTime
-          tmux -S /run/minecraft/smp.sock send-keys \
-            'tellraw @a ["",{"text":"['"$(date -d "@$(cat /tmp/minecraftServerSmpBackupStartTime)" +"%d.%m.%Y %H:%M")"'] ","color":"white"},{"text":"Backup gestartet","color":"dark_red","clickEvent":{"action":"open_url","value":"https://tinyurl.com/n7rn4dbh"},"hoverEvent":{"action":"show_text","contents":[{"text":"Free V-Bucks","color":"aqua"}]}}]' \
-            Enter
-        '';
-      }
-    );
-    backupCleanupCommand = lib.getExe (
-      pkgs.writeShellApplication {
-        name = "backupCleanupCommandSmp";
-        runtimeInputs = with pkgs; [
-          coreutils-full
-          tmux
-        ];
-        text = ''
-          systemctl is-active minecraft-server-smp.service --quiet && \
-          tmux -S /run/minecraft/smp.sock send-keys \
-            'tellraw @a ["",{"text":"['"$(date +"%d.%m.%Y %H:%M")"'] ","color":"white"},{"text":"Backup vollendet","bold":true,"color":"green","hoverEvent":{"action":"show_text","contents":[{"text":"'"$(date -d "@$(( "$(date +%s)" - "$(cat /tmp/minecraftServerSmpBackupStartTime)" ))" +"%M:%S")"'m gebraucht. Das Backup verbraucht nun '"$(du -bsh /var/lib/restic/backups/smp | cut -f1)"'","color":"green"}]}}]' \
-            Enter
-        '';
-      }
-    );
 
     pruneOpts = [
       "--keep-last 3"

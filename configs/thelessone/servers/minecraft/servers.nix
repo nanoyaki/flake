7c6e44a65e8b5a19816f353bd1add52ad41993d7
nanoyaki <hanakretzer@gmail.com>
2025-07-21 12:58:57 +0200

    fix(thelessone/minecraft): limit ram

--- configs/thelessone/servers/minecraft/servers.nix
+++ configs/thelessone/servers/minecraft/servers.nix
@@ -7,9 +7,11 @@
 }:
 
 let
+  aikarsFlags = "-XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true";
+
   defaults = {
     autoStart = true;
-    jvmOpts = "-Xms32G -Xmx32G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true";
+    jvmOpts = "-Xms20G -Xmx20G ${aikarsFlags}";
 
     serverProperties = {
       server-ip = "127.0.0.1";
@@ -210,6 +212,7 @@ in
       smp-creative = lib.recursiveUpdate defaults {
         enable = true;
         package = pkgs.fabricServers.fabric-1_21_7;
+        jvmOpts = "-Xms8G -Xmx8G ${aikarsFlags}";
 
         serverProperties = {
           gamemode = "creative";

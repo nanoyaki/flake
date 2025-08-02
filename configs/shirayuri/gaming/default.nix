712a1ebceb3303df6304acdc9fbb8f8dd2b9d5e1
nanoyaki <hanakretzer@gmail.com>
2025-08-02 14:19:08 +0200

    feat: gamemode renice

--- configs/shirayuri/gaming/default.nix
+++ configs/shirayuri/gaming/default.nix
@@ -87,5 +87,16 @@
       prismlauncher
     ]);
 
-  programs.gamemode.enable = true;
+  programs.gamemode = {
+    enable = true;
+    enableRenice = true;
+
+    settings = {
+      general.renice = 10;
+      custom = {
+        start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
+        end = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
+      };
+    };
+  };
 }

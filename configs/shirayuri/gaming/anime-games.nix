3f73f3da37fb6b92aaed90a25a72e4622d8d7a7d
nanoyaki <hanakretzer@gmail.com>
2025-08-07 19:35:00 +0200

    feat(shirayuri): add icons for anime games

--- configs/shirayuri/gaming/anime-games.nix
+++ configs/shirayuri/gaming/anime-games.nix
@@ -15,11 +15,14 @@ in
         pkg = anime-game-launcher;
         desktopItem = makeDesktopItem {
           name = "anime-game-launcher";
-          desktopName = "An Anime Game Launcher";
+          desktopName = "Anime Game Launcher";
           categories = [ "Game" ];
           exec = "anime-game-launcher";
           genericName = "An Anime Game Launcher";
-          icon = "moe.launcher.an-anime-game-launcher";
+          icon = fetchurl {
+            url = "https://raw.githubusercontent.com/an-anime-team/an-anime-game-launcher/0ea0ec6640da846dfc4aedf4458012f9bb874b41/assets/images/icon.png";
+            hash = "sha256-4ue/zzwJdzBCNyuqFYEZIz1cIVcyHwhyX/jAwxPu93Q=";
+          };
           startupNotify = true;
           startupWMClass = "moe.launcher.an-anime-game-launcher";
           type = "Application";
@@ -36,7 +39,10 @@ in
           categories = [ "Game" ];
           exec = "honkers-railway-launcher";
           genericName = "The Honkers Railway Launcher";
-          icon = "moe.launcher.the-honkers-railway-launcher";
+          icon = fetchurl {
+            url = "https://raw.githubusercontent.com/an-anime-team/the-honkers-railway-launcher/9f3bd39baa41184377126ae01d0611ce615bc6a5/assets/images/icon.png";
+            hash = "sha256-F5SnPEVaU2R/JxN3fsqdD+sdqCn7k7WzdM2Y/8J7XB4=";
+          };
           startupNotify = true;
           startupWMClass = "moe.launcher.the-honkers-railway-launcher";
           type = "Application";
@@ -53,7 +59,10 @@ in
           categories = [ "Game" ];
           exec = "sleepy-launcher";
           genericName = "Sleepy Launcher";
-          icon = "moe.launcher.sleepy-launcher";
+          icon = fetchurl {
+            url = "https://raw.githubusercontent.com/an-anime-team/sleepy-launcher/f91bb11626439c85a07a3f5a84ec8410b0721f40/assets/images/icon.png";
+            hash = "sha256-e5WTalA95+oFzj4n+2PGxO7swEjaSCUDnuqli3LraBo=";
+          };
           startupNotify = true;
           startupWMClass = "moe.launcher.sleepy-launcher";
           type = "Application";

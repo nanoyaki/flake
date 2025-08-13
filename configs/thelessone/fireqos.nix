98033f19dd8041ca61df650088ec1bb0a904529b
nanoyaki <hanakretzer@gmail.com>
2025-08-13 18:57:12 +0200

    feat(thelessone): limit world input traffic only

--- configs/thelessone/fireqos.nix
+++ configs/thelessone/fireqos.nix
@@ -6,12 +6,7 @@
   services.fireqos = {
     enable = true;
     config = ''
-      interface enp6s0 world-in input rate 200mbit
-      interface enp6s0 world-out output rate 20mbit
-        class web
-          match tcp ports 80,443
-
-      interface tailscale0 vpn-out output rate 20mbit
+      interface enp6s0 world-in input rate 250mbit
     '';
   };
 }

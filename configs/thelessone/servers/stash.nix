90cefc871bbff0448594df05502537bdb28f2034
nanoyaki <hanakretzer@gmail.com>
2025-08-14 04:33:39 +0200

    fix(thelessone/stash): use mutably configured values

--- configs/thelessone/servers/stash.nix
+++ configs/thelessone/servers/stash.nix
@@ -6,7 +6,10 @@
 }:
 
 let
+  inherit (lib) optionalString;
+
   cfg = config.services.stash;
+  settingsFile = (pkgs.formats.yaml { }).generate "config.yml" cfg.settings;
 in
 
 {
@@ -18,6 +21,7 @@ in
         "stash/sessionStoreSecret"
         "stash/shoko/user"
         "stash/shoko/pass"
+        "stash/stashboxApiKey"
       ]
       (_: {
         owner = cfg.user;
@@ -57,13 +61,165 @@ in
           path = "/mnt/raid/arr-stack/libraries/adult";
         }
       ];
-      python_path = toString (
+      python_path = lib.getExe (
         pkgs.python313.withPackages (
           pyPkgs: with pyPkgs; [
             requests
           ]
         )
       );
+      stash_boxes = [
+        {
+          name = "StashDB";
+          endpoint = "https://stashdb.org/graphql";
+          apikey = "to-be-replaced-by-out-of-store-file";
+        }
+      ];
+
+      calculate_md5 = true;
+      create_image_clip_from_videos = true;
+
+      menu_items = [
+        "scenes"
+        "images"
+        "groups"
+        "markers"
+        "galleries"
+        "performers"
+        "studios"
+        "tags"
+      ];
+
+      scraper_user_agent = "Mozilla/5.0 (X11; Linux x86_64; rv:139.0) Gecko/20100101 Firefox/139.0";
+      scraper_cdp_path = lib.getExe pkgs.ungoogled-chromium;
+
+      ui = {
+        advancedMode = true;
+        enableMovieBackgroundImage = true;
+        enableStudioBackgroundImage = true;
+        enableTagBackgroundImage = true;
+
+        taskDefaults.generate = {
+          clipPreviews = true;
+          covers = true;
+          imagePreviews = true;
+          imageThumbnails = true;
+          interactiveHeatmapsSpeeds = true;
+          markerImagePreviews = true;
+          markerScreenshots = true;
+          markers = true;
+          overwrite = false;
+          phashes = true;
+          previewOptions = {
+            previewExcludeEnd = "0";
+            previewExcludeStart = "0";
+            previewPreset = "slow";
+            previewSegmentDuration = 0.75;
+            previewSegments = 12;
+          };
+          previews = true;
+          sprites = true;
+          transcodes = true;
+        };
+
+        taskDefaults.scan = {
+          rescan = false;
+          scanGenerateClipPreviews = true;
+          scanGenerateCovers = true;
+          scanGenerateImagePreviews = true;
+          scanGeneratePhashes = true;
+          scanGeneratePreviews = true;
+          scanGenerateSprites = true;
+          scanGenerateThumbnails = true;
+        };
+      };
+
+      defaults.identify_task = {
+        options = {
+          fieldoptions = [
+            {
+              createmissing = null;
+              field = "title";
+              strategy = "OVERWRITE";
+            }
+            {
+              createmissing = true;
+              field = "studio";
+              strategy = "MERGE";
+            }
+            {
+              createmissing = true;
+              field = "performers";
+              strategy = "MERGE";
+            }
+            {
+              createmissing = true;
+              field = "tags";
+              strategy = "MERGE";
+            }
+          ];
+          includemaleperformers = true;
+          setcoverimage = true;
+          setorganized = false;
+          skipmultiplematches = true;
+          skipmultiplematchtag = null;
+          skipsinglenameperformers = true;
+          skipsinglenameperformertag = null;
+        };
+        paths = [ ];
+        sceneids = [ ];
+        sources = [
+          {
+            options = null;
+            source = {
+              scraperid = null;
+              stashboxendpoint = "https://stashdb.org/graphql";
+              stashboxindex = null;
+            };
+          }
+          {
+            options = null;
+            source = {
+              scraperid = "ShokoAPI";
+              stashboxendpoint = null;
+              stashboxindex = null;
+            };
+          }
+          {
+            options = null;
+            source = {
+              scraperid = "AniDB";
+              stashboxendpoint = null;
+              stashboxindex = null;
+            };
+          }
+          {
+            options = null;
+            source = {
+              scraperid = "hanime";
+              stashboxendpoint = null;
+              stashboxindex = null;
+            };
+          }
+          {
+            options = {
+              fieldoptions = [ ];
+              includemaleperformers = null;
+              setcoverimage = null;
+              setorganized = false;
+              skipmultiplematches = true;
+              skipmultiplematchtag = null;
+              skipsinglenameperformers = true;
+              skipsinglenameperformertag = null;
+            };
+            source = {
+              scraperid = "builtin_autotag";
+              stashboxendpoint = null;
+              stashboxindex = null;
+            };
+          }
+        ];
+      };
     };
   };
 
@@ -71,6 +227,38 @@ in
     requires = [ "mnt-raid.mount" ];
     after = [ "mnt-raid.mount" ];
     bindsTo = [ "mnt-raid.mount" ];
+
+    serviceConfig.ExecStartPre = lib.mkForce (
+      pkgs.writers.writeBash "stash-setup.bash" (
+        ''
+          install -d ${cfg.settings.generated}
+          if [[ -z "${toString cfg.mutableSettings}" || ! -f ${cfg.dataDir}/config.yml ]]; then
+            env \
+              password=$(< ${cfg.passwordFile}) \
+              jwtSecretKeyFile=$(< ${cfg.jwtSecretKeyFile}) \
+              sessionStoreKeyFile=$(< ${cfg.sessionStoreKeyFile}) \
+              stashBoxApiKeyFile=$(< ${config.sops.secrets."stash/stashboxApiKey".path}) \
+              ${lib.getExe pkgs.yq-go} '
+                .jwt_secret_key = strenv(jwtSecretKeyFile) |
+                .session_store_key = strenv(sessionStoreKeyFile) |
+                .stash_boxes[0].apikey = strenv(stashBoxApiKeyFile) |
+                (
+                  strenv(password) as $password |
+                  with(select($password != ""); .password = $password)
+                )
+              ' ${settingsFile} > ${cfg.dataDir}/config.yml
+          fi
+        ''
+        + optionalString cfg.mutablePlugins ''
+          install -d ${cfg.settings.plugins_path}
+          ls ${cfg.plugins} | xargs -I{} ln -sf '${cfg.plugins}/{}' ${cfg.settings.plugins_path}
+        ''
+        + optionalString cfg.mutableScrapers ''
+          install -d ${cfg.settings.scrapers_path}
+          ls ${cfg.scrapers} | xargs -I{} ln -sf '${cfg.scrapers}/{}' ${cfg.settings.scrapers_path}
+        ''
+      )
+    );
   };
 
   environment.systemPackages = [ pkgs.chromium ];

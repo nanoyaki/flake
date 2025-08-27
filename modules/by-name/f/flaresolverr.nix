884e44c19fc5599c39dc96731f6979f568dcea57
nanoyaki <hanakretzer@gmail.com>
2025-08-27 10:23:08 +0200

    fix(modules/flaresolverr): fix xvfbwrapper for flaresolverr

--- modules/by-name/f/flaresolverr.nix
+++ modules/by-name/f/flaresolverr.nix
@@ -15,5 +15,22 @@
       enable = true;
       port = 8191;
     };
+
+    nixpkgs.overlays = [
+      (_: prev: {
+        pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
+          (pyFinal: pyPrev: {
+            xvfbwrapper = pyPrev.xvfbwrapper.overridePythonAttrs {
+              format = null;
+              pyproject = true;
+
+              build-system = [ pyFinal.setuptools ];
+
+              doCheck = true;
+            };
+          })
+        ];
+      })
+    ];
   };
 }

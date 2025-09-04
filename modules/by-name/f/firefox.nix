898bcccbd702c69d3bf977462c7ce43a3364cac4
nanoyaki <hanakretzer@gmail.com>
2025-09-04 13:52:46 +0200

    fix: switch to librewolf

--- modules/by-name/f/firefox.nix
+++ modules/by-name/f/firefox.nix
@@ -7,23 +7,10 @@
 }:
 
 let
-  inherit (lib) genAttrs mkIf;
+  inherit (lib) mkIf;
   inherit (lib'.options) mkFalseOption;
 
   icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-
-  defaultApplications = genAttrs [
-    "text/html"
-    "text/css"
-    "text/xml"
-    "application/xhtml+xml"
-    "application/xml"
-    "application/atom+xml"
-    "application/rss+xml"
-    "application/pdf"
-    "x-scheme-handler/http"
-    "x-scheme-handler/https"
-  ] (_: "firefox.desktop");
 in
 
 {
@@ -31,7 +18,15 @@ in
 
   config = mkIf config.config'.firefox.enable {
     programs.firefox = {
+      enable = true;
+      package = pkgs.librewolf;
       wrapperConfig.pipewireSupport = true;
+
+      policies = {
+        DontCheckDefaultBrowser = true;
+        DisablePocket = true;
+        DisableAppUpdate = true;
+      };
 
       preferences = {
         "widget.use-xdg-desktop-portal.file-picker" = 1;
@@ -46,128 +41,131 @@ in
 
     environment.sessionVariables.BROWSER = config.programs.firefox.package.meta.mainProgram;
 
-    xdg.mime = { inherit defaultApplications; };
-    hms = [
-      {
-        xdg.mimeApps = { inherit defaultApplications; };
-        programs.firefox = {
-          enable = true;
-          inherit (config.programs.firefox) package;
+    xdg.mime.addedAssociations = {
+      "text/html" = "LibreWolf.desktop";
+      "application/pdf" = "LibreWolf.desktop";
+      "x-scheme-handler/http" = "LibreWolf.desktop";
+      "x-scheme-handler/https" = "LibreWolf.desktop";
+    };
 
-          profiles.default = {
-            id = 0;
-            name = "default";
-            isDefault = true;
-            search = {
-              force = true;
-              default = "google";
-              privateDefault = "ddg";
-              engines = {
-                "Nix Packages" = {
-                  urls = [
-                    {
-                      template = "https://search.nixos.org/packages";
-                      params = [
-                        {
-                          name = "channel";
-                          value = "unstable";
-                        }
-                        {
-                          name = "query";
-                          value = "{searchTerms}";
-                        }
-                      ];
-                    }
-                  ];
+    hms = lib.singleton {
+      programs.firefox = {
+        enable = true;
+        inherit (config.programs.firefox) package;
 
-                  inherit icon;
-                  definedAliases = [ "@np" ];
-                };
+        profiles.default = {
+          id = 0;
+          name = "default";
+          isDefault = true;
+          search = {
+            force = true;
+            default = "google";
+            privateDefault = "ddg";
+            engines = {
+              "Nix Packages" = {
+                urls = [
+                  {
+                    template = "https://search.nixos.org/packages";
+                    params = [
+                      {
+                        name = "channel";
+                        value = "unstable";
+                      }
+                      {
+                        name = "query";
+                        value = "{searchTerms}";
+                      }
+                    ];
+                  }
+                ];
 
-                "Nix Options" = {
-                  urls = [
-                    {
-                      template = "https://search.nixos.org/options";
-                      params = [
-                        {
-                          name = "channel";
-                          value = "unstable";
-                        }
-                        {
-                          name = "query";
-                          value = "{searchTerms}";
-                        }
-                      ];
-                    }
-                  ];
+                inherit icon;
+                definedAliases = [ "@np" ];
+              };
 
-                  inherit icon;
-                  definedAliases = [ "@no" ];
-                };
+              "Nix Options" = {
+                urls = [
+                  {
+                    template = "https://search.nixos.org/options";
+                    params = [
+                      {
+                        name = "channel";
+                        value = "unstable";
+                      }
+                      {
+                        name = "query";
+                        value = "{searchTerms}";
+                      }
+                    ];
+                  }
+                ];
 
-                "NixOS Wiki" = {
-                  urls = [
-                    {
-                      template = "https://wiki.nixos.org/w/index.php";
-                      params = [
-                        {
-                          name = "search";
-                          value = "{searchTerms}";
-                        }
-                      ];
-                    }
-                  ];
+                inherit icon;
+                definedAliases = [ "@no" ];
+              };
 
-                  inherit icon;
-                  definedAliases = [ "@nw" ];
-                };
+              "NixOS Wiki" = {
+                urls = [
+                  {
+                    template = "https://wiki.nixos.org/w/index.php";
+                    params = [
+                      {
+                        name = "search";
+                        value = "{searchTerms}";
+                      }
+                    ];
+                  }
+                ];
+
+                inherit icon;
+                definedAliases = [ "@nw" ];
               };
             };
-            extensions = {
-              force = true;
-              packages = with pkgs.nur.repos.rycee.firefox-addons; [
-                ublock-origin
-                keepa
-                refined-github
-                languagetool
-                control-panel-for-twitter
-                tampermonkey
-                redirector
-                reddit-enhancement-suite
-                mullvad
-
-                steam-database
-                augmented-steam
-
-                return-youtube-dislikes
-                youtube-screenshot-button
-
-                seventv
-                betterttv
-                twitch-auto-points
-              ];
-            };
           };
+          extensions = {
+            force = true;
+            packages = with pkgs.nur.repos.rycee.firefox-addons; [
+              ublock-origin
+              keepa
+              refined-github
+              languagetool
+              control-panel-for-twitter
+              tampermonkey
+              redirector
+              reddit-enhancement-suite
+              mullvad
 
-          profiles.vpn = {
-            id = 1;
-            name = "vpn";
-            isDefault = false;
-            search = {
-              force = true;
-              default = "google";
-              privateDefault = "ddg";
-            };
-            extensions = {
-              force = true;
-              packages = with pkgs.nur.repos.rycee.firefox-addons; [
-                ublock-origin
-                mullvad
-              ];
-            };
+              steam-database
+              augmented-steam
+
+              return-youtube-dislikes
+              youtube-screenshot-button
+
+              seventv
+              betterttv
+              twitch-auto-points
+            ];
           };
         };
-      }
-    ];
+
+        profiles.vpn = {
+          id = 1;
+          name = "vpn";
+          isDefault = false;
+          search = {
+            force = true;
+            default = "google";
+            privateDefault = "ddg";
+          };
+          extensions = {
+            force = true;
+            packages = with pkgs.nur.repos.rycee.firefox-addons; [
+              ublock-origin
+              mullvad
+            ];
+          };
+        };
+      };
+    };
   };
 }

229f4216b91e1a07ce6ee12cf0800b960c720b5a
nanoyaki <hanakretzer@gmail.com>
2025-09-16 23:52:42 +0200

    fix(thelessone/immich): upgrade to 1.142.1 for sync fixes

--- configs/thelessone/servers/immich.nix
+++ configs/thelessone/servers/immich.nix
@@ -1,7 +1,32 @@
-{ config, ... }:
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
 
 {
   config'.immich.enable = true;
+
+  services.immich.package = pkgs.immich.overrideAttrs (
+    oldAttrs:
+    lib.optionalAttrs (lib.versionOlder oldAttrs.version "1.142.1") rec {
+      version = "1.142.1";
+      src = pkgs.fetchFromGitHub {
+        owner = "immich-app";
+        repo = "immich";
+        tag = "v${version}";
+        hash = "sha256-u538GWupnkH2K81Uk9yEuHc3pAeVexnJOnhWo7gElL0=";
+      };
+
+      pnpmDeps = pkgs.pnpm_10.fetchDeps {
+        pname = "immich";
+        inherit version src;
+        fetcherVersion = 2;
+        hash = "sha256-aYG5SpFZxhbz32YAdP39RYwn2GV+mFWhddd4IFuPuz8=";
+      };
+    }
+  );
 
   services.immich-public-proxy = {
     enable = true;

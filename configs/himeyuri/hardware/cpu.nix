30b4d3799ee58469a8a6e4019d25fa6279694947
nanoyaki <hanakretzer@nanoyaki.space>
2025-09-27 15:36:26 +0200

    feat(himeyuri): undervolt cpu

--- configs/himeyuri/hardware/cpu.nix
+++ configs/himeyuri/hardware/cpu.nix
@@ -1,5 +1,15 @@
+{ inputs, ... }:
+
 {
+  imports = [ inputs.vermeer-undervolt.nixosModules.vermeer-undervolt ];
+
   hardware.cpu.amd.ryzen-smu.enable = true;
   hardware.cpu.amd.updateMicrocode = true;
   services.power-profiles-daemon.enable = true;
+
+  services.vermeer-undervolt = {
+    enable = true;
+    cores = 8;
+    milivolts = 30;
+  };
 }

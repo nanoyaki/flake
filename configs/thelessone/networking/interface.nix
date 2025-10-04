7356fe7685b4b65ad379519d304f1be7053141d8
nanoyaki <hanakretzer@nanoyaki.space>
2025-10-04 01:57:50 +0000

    fix(thelessone/networking): just use DHCP for v6

--- configs/thelessone/networking/interface.nix
+++ configs/thelessone/networking/interface.nix
@@ -1,16 +1,8 @@
 {
   networking = {
+    useDHCP = true;
+
     interfaces.enp6s0 = {
-      ipv6.addresses = [
-        {
-          address = "2001:4bb8:1ce:e329::2";
-          prefixLength = 64;
-        }
-        {
-          address = "fd6f:9046:1f17::2";
-          prefixLength = 64;
-        }
-      ];
       ipv4.addresses = [
         {
           address = "10.0.0.5";
@@ -21,10 +13,6 @@
 
     defaultGateway = {
       address = "10.0.0.1";
-      interface = "enp6s0";
-    };
-    defaultGateway6 = {
-      address = "fe80::6b4:feff:fe15:19e5";
       interface = "enp6s0";
     };
   };

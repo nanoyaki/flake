ba3fb003178561276f60aea0d5ff3c4d7c1cc39e
nanoyaki <hanakretzer@nanoyaki.space>
2025-10-05 14:56:04 +0000

    fix(yuri/samba): make it work again lol

--- configs/yuri/servers/samba.nix
+++ configs/yuri/servers/samba.nix
@@ -2,33 +2,24 @@
 
 {
   sops.secrets."users/hana".neededForUsers = true;
+  sops.secrets."users/hana-smb" = { };
 
-  users.users.nas.extraGroups = [ "nas" ];
   users.groups.nas = { };
+  users.users.nas.extraGroups = [ "nas" ];
 
-  users.groups.hana = { };
   users.users.hana = {
     isNormalUser = true;
-    group = "hana";
-    createHome = false;
-    home = "/mnt/shares/Hana";
-    homeMode = "755";
-    useDefaultShell = true;
     hashedPasswordFile = config.sops.secrets."users/hana".path;
+    extraGroups = [ "nas" ];
+
+    home = "/mnt/nvme-raid-1/shares/Hana";
+    homeMode = "700";
   };
 
-  systemd.tmpfiles.settings."10-samba" = {
-    "/mnt/nvme-raid-1/shares/Public".d = {
-      user = "nas";
-      group = "nas";
-      mode = "0755";
-    };
-
-    "/mnt/nvme-raid-1/shares/Hana".d = {
-      user = "hana";
-      group = "hana";
-      mode = "0755";
-    };
+  systemd.tmpfiles.settings."10-samba"."/mnt/nvme-raid-1/shares/Public".d = {
+    user = "nas";
+    group = "nas";
+    mode = "0755";
   };
 
   services.samba = {
@@ -37,40 +28,63 @@
 
     settings = {
       global = {
-        security = "user";
-        "netbios name" = "nas";
+        "log level" = 1;
+        "logging" = "systemd";
+
+        "netbios name" = "NAS";
         "server string" = "NAS";
 
         "hosts allow" = "10.0.0. 127.0.0.1 localhost";
         "hosts deny" = "0.0.0.0/0";
 
+        security = "user";
         "guest account" = "nobody";
         "map to guest" = "bad user";
       };
 
       public = {
         path = "/mnt/nvme-raid-1/shares/Public";
+
+        public = "yes";
         browseable = "yes";
         "read only" = "no";
         "guest ok" = "yes";
+
         "create mask" = "0644";
         "directory mask" = "0755";
+
         "force user" = "nas";
         "force group" = "nas";
       };
 
       hana = {
         path = "/mnt/nvme-raid-1/shares/Hana";
-        "browseable" = "yes";
+
+        browseable = "yes";
+        writable = "yes";
         "read only" = "no";
         "guest ok" = "no";
+
         "create mask" = "0640";
         "directory mask" = "0750";
-        "force user" = "hana";
-        "force group" = "hana";
+
         "valid users" = "hana";
+        "force user" = "hana";
+        "force group" = "users";
       };
     };
+  };
+
+  systemd.services.samba-smbd.postStart = ''
+    ( \
+      echo $(< ${config.sops.secrets."users/hana-smb".path}); \
+      echo $(< ${config.sops.secrets."users/hana-smb".path}) \
+    ) | ${config.services.samba.package}/bin/smbpasswd -s -a hana
+  '';
+
+  services.samba-wsdd = {
+    enable = true;
+    openFirewall = true;
   };
 
   services.avahi = {
@@ -81,11 +95,5 @@
     publish.userServices = true;
 
     nssmdns4 = true;
-  };
-
-  services.samba-wsdd = {
-    enable = true;
-    openFirewall = true;
-    hostname = "NAS";
   };
 }

5c9f914b3977bc74582e4f190c4d2b1a506126b2
nanoyaki <hanakretzer@nanoyaki.space>
2025-11-10 00:35:01 +0100

    feat: add wg1

--- configs/shirayuri/wireguard.nix
+++ configs/shirayuri/wireguard.nix
@@ -4,25 +4,50 @@
 }:
 
 {
-  sops.secrets.wg0 = { };
+  sops.secrets = {
+    wg0 = { };
+    wg1 = { };
+  };
 
-  networking.wg-quick.interfaces.wg0 = {
-    address = [
-      "10.101.0.2/32"
-      "fd10::2/128"
-    ];
-    privateKeyFile = config.sops.secrets.wg0.path;
+  networking.wg-quick.interfaces = {
+    wg0 = {
+      address = [
+        "10.101.0.2/32"
+        "fd10::2/128"
+      ];
+      privateKeyFile = config.sops.secrets.wg0.path;
 
-    peers = [
-      {
-        publicKey = "kdBOsYomUk9YEFs+qSsKHnbaMAL6r57IlkJoNweRKj8=";
-        endpoint = "hanakretzer.de:51820";
-        allowedIPs = [
-          "10.101.0.1/32"
-          "fd10::1/128"
-        ];
-        persistentKeepalive = 25;
-      }
-    ];
+      peers = [
+        {
+          publicKey = "kdBOsYomUk9YEFs+qSsKHnbaMAL6r57IlkJoNweRKj8=";
+          endpoint = "hanakretzer.de:51820";
+          allowedIPs = [
+            "10.101.0.1/32"
+            "fd10::1/128"
+          ];
+          persistentKeepalive = 25;
+        }
+      ];
+    };
+
+    wg1 = {
+      address = [
+        "100.64.64.7/32"
+        "fd64::7/128"
+      ];
+      privateKeyFile = config.sops.secrets.wg1.path;
+
+      peers = [
+        {
+          publicKey = "JB0jviICHpiTm1PYjm4+FCWCPLAjU/NZBm6tRO6/XGY=";
+          endpoint = "theless.one:51820";
+          allowedIPs = [
+            "100.64.64.1/32"
+            "fd64::1/128"
+          ];
+          persistentKeepalive = 25;
+        }
+      ];
+    };
   };
 }
--- configs/yamayuri/wireguard.nix
+++ configs/yamayuri/wireguard.nix
@@ -4,32 +4,57 @@
 }:
 
 {
-  sops.secrets.wg0 = { };
+  sops.secrets = {
+    wg0 = { };
+    wg1 = { };
+  };
 
-  networking.wg-quick.interfaces.wg0 = {
-    address = [
-      "10.101.0.1/24"
-      "fd10::1/64"
-    ];
-    listenPort = 51820;
-    privateKeyFile = config.sops.secrets.wg0.path;
+  networking.wg-quick.interfaces = {
+    wg0 = {
+      address = [
+        "10.101.0.1/24"
+        "fd10::1/64"
+      ];
+      listenPort = 51820;
+      privateKeyFile = config.sops.secrets.wg0.path;
 
-    peers = [
-      {
-        publicKey = "95xHXn8LjJ3wR7pSPDIyH2j7VGu38egMPJYJoWSgQDw=";
-        allowedIPs = [
-          "10.101.0.2/32"
-          "fd10::2/128"
-        ];
-      }
-      {
-        publicKey = "Vaq/AzMYsYq0Ba1MS75uyqTJuYk+L5jHUv67ms3jdgQ=";
-        allowedIPs = [
-          "10.101.0.3/32"
-          "fd10::3/128"
-        ];
-      }
-    ];
+      peers = [
+        {
+          publicKey = "95xHXn8LjJ3wR7pSPDIyH2j7VGu38egMPJYJoWSgQDw=";
+          allowedIPs = [
+            "10.101.0.2/32"
+            "fd10::2/128"
+          ];
+        }
+        {
+          publicKey = "Vaq/AzMYsYq0Ba1MS75uyqTJuYk+L5jHUv67ms3jdgQ=";
+          allowedIPs = [
+            "10.101.0.3/32"
+            "fd10::3/128"
+          ];
+        }
+      ];
+    };
+
+    wg1 = {
+      address = [
+        "100.64.64.6/32"
+        "fd64::6/128"
+      ];
+      privateKeyFile = config.sops.secrets.wg1.path;
+
+      peers = [
+        {
+          publicKey = "JB0jviICHpiTm1PYjm4+FCWCPLAjU/NZBm6tRO6/XGY=";
+          endpoint = "theless.one:51820";
+          allowedIPs = [
+            "100.64.64.1/32"
+            "fd64::1/128"
+          ];
+          persistentKeepalive = 25;
+        }
+      ];
+    };
   };
 
   boot.kernel.sysctl = {

8679a8abfd63f329da667312e4fa0a36f12ec47f
nanoyaki <hanakretzer@nanoyaki.space>
2025-11-11 13:56:57 +0100

    feat(shirayuri): add droidcam

--- configs/shirayuri/configuration.nix
+++ configs/shirayuri/configuration.nix
@@ -146,4 +146,9 @@ in
     enable = true;
     openFirewall = true;
   };
+
+  programs.droidcam.enable = true;
+  boot.extraModprobeConfig = ''
+    options v4l2loopback video_nr=0 width=1920 max_width=1920 height=1080 max_height=1080 format=YU12 exclusive_caps=1 card_label=Phone debug=1
+  '';
 }

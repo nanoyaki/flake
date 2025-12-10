93681d37d150e368ee627833efc1f4c466123acc
nanoyaki <hanakretzer@nanoyaki.space>
2025-12-10 19:25:09 +0100

    feat(shirayuri): add pikvm connection

--- configs/shirayuri/wireguard.nix
+++ configs/shirayuri/wireguard.nix
@@ -7,6 +7,7 @@
   sops.secrets = {
     wg0 = { };
     wg1 = { };
+    wg2 = { };
   };
 
   networking.wg-quick.interfaces = {
@@ -45,6 +46,20 @@
             "100.64.64.1/32"
             "fd64::1/128"
           ];
+          persistentKeepalive = 25;
+        }
+      ];
+    };
+
+    wg2 = {
+      address = [ "10.200.200.2/32" ];
+      privateKeyFile = config.sops.secrets.wg2.path;
+
+      peers = [
+        {
+          publicKey = "Pd934yDpHcc2pdv4eV2YBQYGgncW/yacNHtoNQsA5wM=";
+          endpoint = "theless.one:51821";
+          allowedIPs = [ "10.200.200.1/32" ];
           persistentKeepalive = 25;
         }
       ];

b752ca9356fd3adf321b00ee844bb73c59b72bde
nanoyaki <hanakretzer@gmail.com>
2025-12-23 11:03:12 +0000

    feat(kuroyuri): add ausweisapp

--- configs/kuroyuri/configuration.nix
+++ configs/kuroyuri/configuration.nix
@@ -23,6 +23,16 @@
 
   programs.thunderbird.enable = true;
 
+  programs.ausweisapp = {
+    enable = true;
+    openFirewall = true;
+  };
+
+  stylix.image = pkgs.fetchurl {
+    url = "https://files.theless.one/share/kuroyuri-wallpaper/wallpaper.png";
+    hash = "sha256-OQ7qUIFdWb/LYdKNtXlQWFj2A0DL3GltxotmFkO9oTs=";
+  };
+
   hm.home.file.".face.icon".source = pkgs.fetchurl {
     url = "https://cdn.bsky.app/img/avatar/plain/did:plc:majihettvb7ieflgmkvujecu/bafkreib6be5oip6rht4vqnmldx5hzulr6irh55yarwbmxt2us2imfoiyd4@png";
     hash = "sha256-mQ8il+zU30EAxFAulUFkkXvYs9gubKCeQtaYRyJNXJ8=";

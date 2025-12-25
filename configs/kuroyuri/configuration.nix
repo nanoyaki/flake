26e3ef364c055a3c6f08baad54fc6471b3dd317a
nanoyaki <hanakretzer@nanoyaki.space>
2025-12-25 19:46:35 +0100

    fix: comment out stylix code

--- configs/kuroyuri/configuration.nix
+++ configs/kuroyuri/configuration.nix
@@ -28,10 +28,10 @@
     openFirewall = true;
   };
 
-  stylix.image = pkgs.fetchurl {
-    url = "https://files.theless.one/share/kuroyuri-wallpaper/wallpaper.png";
-    hash = "sha256-OQ7qUIFdWb/LYdKNtXlQWFj2A0DL3GltxotmFkO9oTs=";
-  };
+  # stylix.image = pkgs.fetchurl {
+  #   url = "https://files.theless.one/share/kuroyuri-wallpaper/wallpaper.png";
+  #   hash = "sha256-OQ7qUIFdWb/LYdKNtXlQWFj2A0DL3GltxotmFkO9oTs=";
+  # };
 
   hm.home.file.".face.icon".source = pkgs.fetchurl {
     url = "https://cdn.bsky.app/img/avatar/plain/did:plc:majihettvb7ieflgmkvujecu/bafkreib6be5oip6rht4vqnmldx5hzulr6irh55yarwbmxt2us2imfoiyd4@png";

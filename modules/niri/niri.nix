0f72c06253217766a2d466d81a0acc849240b091
nanoyaki <contact@nanoyaki.space>
2026-09-17 01:24:13 +0200

    fix: screenshare breaking variable according to niri docs

--- modules/niri/niri.nix
+++ modules/niri/niri.nix
@@ -316,7 +316,6 @@ in
 
         environment = {
           CLUTTER_BACKEND = "wayland";
-          GDK_BACKEND = "wayland";
           MOZ_ENABLE_WAYLAND = "1";
           NIXOS_OZONE_WL = "1";
           QT_QPA_PLATFORM = "wayland";
